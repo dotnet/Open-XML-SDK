f0fb5df99fdfb2342bac4237d0db918ca1f9e451
ThomasBarnekow <thomas@barnekow.info>
2014-09-08 09:23:25 +0200

    Optimized SetAssemblyVersionString.ps1. Now using UTC and touching AssemblyInfo.cs at most once per day, which avoids unnecessary builds that are performed when touching AssemblyInfo.cs without actually changing the version string.

--- SetAssemblyVersionString.ps1
+++ SetAssemblyVersionString.ps1
@@ -8,32 +8,56 @@ function GetVersionString
 
     # This script will always result in a version number greater than this.
     # This script hard codes major version = 2, minor version = 5.
-	# The version number will only depend on the hour and not on the minute as
-	# defined in the original script, because the Debug and Release DLLs would
-	# typically have different version strings even in a batch build. 
-	# By ignoring minutes, DLLs produced during the same hour will have the 
-	# same version string.
+    # The version number will only depend on the day and not on the time as
+    # defined in the original script, because the Debug and Release DLLs would
+    # typically have different version strings even in a batch build. 
+    # By ignoring hours and minutes, DLLs produced on the same day will have the 
+    # same version string.
 
     $major = [int]2;
     $minor = [int]5;
 
-    $nowDate = [DateTime]::Now
+    $nowDate = [DateTime]::UtcNow
     $referenceDate = New-Object DateTime(2014, 6, 1);
     $delta = [TimeSpan]($nowDate - $referenceDate);
     $third = [int]($delta.TotalDays + 5632);
-    # $fourth = [int]($nowDate.Hour * 60 + $nowDate.Minute);
-    $fourth = [int]($nowDate.Hour * 60);
+    $fourth = 0;
     $versionString = [string]::Format("{0}.{1}.{2}.{3}", $major, $minor, $third, $fourth);
     return $versionString
 }
 
+function WriteAssemblyVersionString($versionString)
+{
+    # Produce new AssemblyInfo.cs using version string
+    gc .\src\ofapi\Properties\AssemblyInfo.cs.source | % { $_ -replace "2.0.3302.0", "$versionString" } > .\src\ofapi\Properties\AssemblyInfo.cs
+
+    # Remember last used version string
+    sc .\build\version.txt $versionString
+}
+
 function SetAssemblyVersionString
 {
-	# Get new version string.
-	$paramVersion = GetVersionString
+    # Compute new version string.
+    $computedVersion = GetVersionString
 
-	# Produce new AssemblyInfo.cs using new version string.
-    gc .\src\ofapi\Properties\AssemblyInfo.cs.source | % { $_ -replace "2.0.3302.0", "$paramVersion" } > .\src\ofapi\Properties\AssemblyInfo.cs
+    if (test-path .\build\version.txt)
+    {
+        # version.txt file exists, so read last-used version string
+        $lastVersion = gc .\build\version.txt
+
+        # Write AssemblyInfo.cs if computed version is different from last used version
+        if ($lastVersion -ne $computedVersion)
+        {
+            WriteAssemblyVersionString $computedVersion
+        }
+    }
+    else
+    {
+        # version.txt file does not exist, so write AssemblyInfo.cs
+        WriteAssemblyVersionString $computedVersion
+
+    }    
+
 }
 
 . SetAssemblyVersionString
