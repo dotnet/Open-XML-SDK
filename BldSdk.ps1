bc76c70c35e2f6ff1a19f0c5b089a626bdc1d6d7
Eric White <eric@ericwhite.com>
2015-01-14 05:25:12 +0530

    Add BOM

--- BldSdk.ps1
+++ BldSdk.ps1
@@ -1,4 +1,4 @@
-"PowerShell script for building the Open XML SDK - June 22, 2014"
+﻿"PowerShell script for building the Open XML SDK - June 22, 2014"
 "Copyright (c) Microsoft Open Technologies, Inc.  All rights reserved."
 "Licensed under the Apache License, Version 2.0."
 "See License.txt in the project root for license information."
