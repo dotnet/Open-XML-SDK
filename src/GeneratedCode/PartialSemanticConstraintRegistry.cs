1c5f27000f3b0e49bf353dedf90ee52b2a872caa
Eric White <eric@ericwhite.com>
2015-01-14 05:52:06 +0530

    Remove BOM from generated files

--- src/GeneratedCode/PartialSemanticConstraintRegistry.cs
+++ src/GeneratedCode/PartialSemanticConstraintRegistry.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Open Technologies, Inc.  All rights reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+// Copyright (c) Microsoft Open Technologies, Inc.  All rights reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 // This source file is machine generated.  Please do not change the code manually.
 using System;
 using System.Collections.Generic;
