c7536c01b039737fdb313044aa1a1c711e201b56
Sergey Tihon <sergey.tihon@gmail.com>
2016-03-06 22:20:33 +0300

    Migration to xUnit 2.1

--- build.open-xml-sdk.cmd
+++ build.open-xml-sdk.cmd
@@ -14,5 +14,5 @@ if exist "%SystemRoot%\Microsoft.NET\Framework64" (
 "%FrameworkDir%\%FrameworkVersion%\msbuild.exe" /p:Configuration=Release Open-XML-SDK.sln
 
 REM Run tests
-packages\xunit.runner.console.2.0.0\tools\xunit.console.exe System.IO.Packaging.Tests\bin\Release\System.IO.Packaging.Tests.dll
-packages\xunit.runner.console.2.0.0\tools\xunit.console.exe DocumentFormat.OpenXml.Tests\bin\Release\DocumentFormat.OpenXml.Tests.dll
+packages\xunit.runner.console.2.1.0\tools\xunit.console.exe System.IO.Packaging.Tests\bin\Release\System.IO.Packaging.Tests.dll -verbose
+packages\xunit.runner.console.2.1.0\tools\xunit.console.exe DocumentFormat.OpenXml.Tests\bin\Release\DocumentFormat.OpenXml.Tests.dll -verbose
