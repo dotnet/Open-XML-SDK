f752fcec9ab6f1b59543fd51437cc79cd69418b3
Sergey Tihon <sergey.tihon@gmail.com>
2016-03-13 23:34:01 +0300

    mono cert import

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
-  mono .nuget/nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion -Source "https://nuget.org/api/v2/"
-fi
-
 if [[ "$OS" != "Windows_NT" ]] &&
        [ ! -e ~/.config/.mono/certs ]
 then
   mozroots --import --sync --quiet
+fi
+
+if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
+  mono .nuget/nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion -Source "https://nuget.org/api/v2/"
 fi
 
 #workaround assembly resolution issues in build.fsx
