c5153df79788646e2d032278f7221df3611a7191
Taylor Southwick <tasou@microsoft.com>
2016-10-18 15:31:15 -0700

    Replace WinForms style resources with embedded resource

---
+++ DocumentFormat.OpenXml/src/ofapi/Validation/ValidationResources.Objects.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace DocumentFormat.OpenXml.Validation
+{
+    internal partial class ValidationResources
+    {
+        private static byte[] GetEmbeddedResource([CallerMemberName]string name = null)
+        {
+            var names = typeof(ValidationResources).GetTypeInfo().Assembly.GetManifestResourceNames();
+            using (var stream = typeof(ValidationResources).GetTypeInfo().Assembly.GetManifestResourceStream($"DocumentFormat.OpenXml.src.GeneratedCode.{name}.bin"))
+            using (var ms = new MemoryStream())
+            {
+                stream.CopyTo(ms);
+
+                return ms.ToArray();
+            }
+        }
+
+        internal static byte[] O12SchemaConstraintDatas => GetEmbeddedResource();
+
+        internal static byte[] O14SchemaConstraintDatas => GetEmbeddedResource();
+
+        internal static byte[] O15SchemaConstraintDatas => GetEmbeddedResource();
+    }
+}
