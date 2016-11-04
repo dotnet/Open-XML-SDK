17559eaf0cd0aba38664ff1254cd32350c4e4e4a
Taylor Southwick <tasou@microsoft.com>
2016-11-03 17:27:06 -0700

    Switch schema datas to use stream

--- DocumentFormat.OpenXml/src/ofapi/Validation/ValidationResources.Objects.cs
+++ DocumentFormat.OpenXml/src/ofapi/Validation/ValidationResources.Objects.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -6,22 +7,31 @@ namespace DocumentFormat.OpenXml.Validation
 {
     internal partial class ValidationResources
     {
-        private static byte[] GetEmbeddedResource([CallerMemberName]string name = null)
+        public static Stream GetSchemaStream(FileFormatVersions fileFormat)
         {
-            var names = typeof(ValidationResources).GetTypeInfo().Assembly.GetManifestResourceNames();
-            using (var stream = typeof(ValidationResources).GetTypeInfo().Assembly.GetManifestResourceStream($"DocumentFormat.OpenXml.src.GeneratedCode.{name}.bin"))
-            using (var ms = new MemoryStream())
+            switch (fileFormat)
             {
-                stream.CopyTo(ms);
-
-                return ms.ToArray();
+                case FileFormatVersions.Office2007:
+                    return O12SchemaConstraintDatas;
+                case FileFormatVersions.Office2010:
+                    return O14SchemaConstraintDatas;
+                case FileFormatVersions.Office2013:
+                    return O15SchemaConstraintDatas;
+                default:
+                    Debug.Assert(fileFormat == FileFormatVersions.Office2007 || fileFormat == FileFormatVersions.Office2010 || fileFormat == FileFormatVersions.Office2013);
+                    return O12SchemaConstraintDatas;
             }
         }
 
-        internal static byte[] O12SchemaConstraintDatas => GetEmbeddedResource();
+        private static Stream GetEmbeddedResource([CallerMemberName]string name = null)
+        {
+            return typeof(ValidationResources).GetTypeInfo().Assembly.GetManifestResourceStream($"DocumentFormat.OpenXml.src.GeneratedCode.{name}.bin");
+        }
 
-        internal static byte[] O14SchemaConstraintDatas => GetEmbeddedResource();
+        private static Stream O12SchemaConstraintDatas => GetEmbeddedResource();
 
-        internal static byte[] O15SchemaConstraintDatas => GetEmbeddedResource();
+        private static Stream O14SchemaConstraintDatas => GetEmbeddedResource();
+
+        private static Stream O15SchemaConstraintDatas => GetEmbeddedResource();
     }
 }
