32a92cdc37a24585de83031023353e0497e20fef
Taylor Southwick <tasou@microsoft.com>
2016-11-05 20:40:11 -0700

    Add appveyor CI/CD builds
    This adds support for providing CI and CD builds via the .NET Foundation
    signing service and pushing it to a myget feed

--- Clean.ps1
+++
@@ -1,6 +0,0 @@
-ri -Recurse .\DocumentFormat.OpenXml.Tests\bin
-ri -Recurse .\DocumentFormat.OpenXml.Tests\obj
-ri -Recurse .\DocumentFormat.OpenXml.WB.Tests\bin
-ri -Recurse .\DocumentFormat.OpenXml.WB.Tests\obj
-ri -Recurse .\System.IO.Packaging.Tests\bin
-ri -Recurse .\System.IO.Packaging.Tests\obj
