de09300335af095f034f501f7b743ca0010bcf13
Taylor Southwick <tasou@microsoft.com>
2017-10-12 21:45:32 -0700

    Ensure all files are UTF8 with BOM

--- DocumentFormat.OpenXml.Tests/ConformanceTest/Guide/GeneratedDocument.cs
+++ DocumentFormat.OpenXml.Tests/ConformanceTest/Guide/GeneratedDocument.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Open Technologies, Inc.  All rights reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+﻿// Copyright (c) Microsoft Open Technologies, Inc.  All rights reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Presentation;
 using P = DocumentFormat.OpenXml.Presentation;
