34fb9952acbacca7fe3d52e29e793b31513315d1
Taylor Southwick <tasou@microsoft.com>
2017-11-01 13:46:03 -0700

    Remove file access from timeline (#282)

--- DocumentFormat.OpenXml.Tests/ConformanceTest/Timeline/TimeLineTest.cs
+++ DocumentFormat.OpenXml.Tests/ConformanceTest/Timeline/TimeLineTest.cs
@@ -1,66 +1,30 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc.  All rights reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
-using System;
+
+using DocumentFormat.OpenXml.Tests.TimelineClass;
 using System.IO;
+using Xunit;
 
 namespace DocumentFormat.OpenXml.Tests.TimeLine
 {
-    using DocumentFormat.OpenXml.Tests.TaskLibraries;
-    using DocumentFormat.OpenXml.Tests.TimelineClass;
-    using OxTest;
-    using Xunit;
-    using Xunit.Abstractions;
-
-    /// <summary>
-    /// Tests for TimeLine elements
-    /// </summary>
-    public class TimeLineTest : OpenXmlTestBase
+    public class TimeLineTest
     {
-        private readonly string generateDocumentFile = Path.Combine(TestUtil.TestResultsDirectory, Guid.NewGuid().ToString() + ".xlsx");
-        private readonly string editeDocumentFile = Path.Combine(TestUtil.TestResultsDirectory, Guid.NewGuid().ToString() + ".xlsx");
-        private readonly string deleteTimelineStyleDocumentFile = Path.Combine(TestUtil.TestResultsDirectory, Guid.NewGuid().ToString() + ".xlsx");
-        private readonly string addTimelineStyleDocumentFile = Path.Combine(TestUtil.TestResultsDirectory, Guid.NewGuid().ToString() + ".xlsx");
-        private readonly TestEntities testEntities = null;
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public TimeLineTest(ITestOutputHelper output)
-            : base(output)
-        {
-            string createFilePath = this.GetTestFilePath(this.generateDocumentFile);
-            GeneratedDocument generatedDocument = new GeneratedDocument();
-            generatedDocument.CreatePackage(createFilePath);
-
-            this.Log.Pass("Create Excel file. File path=[{0}]", createFilePath);
-
-            this.testEntities = new TestEntities(createFilePath);
-        }
-
-        /// <summary>
-        /// Attribute editing test for TimeLine Control"
-        /// </summary>
         [Fact]
-        public void TimeLine01EditDeleteAddAttribute()
+        public void TimeLineEditAttributes()
         {
-            string originalFilepath = this.GetTestFilePath(this.generateDocumentFile);
-            string editFilePath = this.GetTestFilePath(this.editeDocumentFile);
-            string deleteTimelineStyleFilePath = this.GetTestFilePath(this.deleteTimelineStyleDocumentFile);
-            string addTimelineStyleFilePath = this.GetTestFilePath(addTimelineStyleDocumentFile);
+            using (var ms = new MemoryStream())
+            {
+                GeneratedDocument.CreatePackage(ms);
 
-            System.IO.File.Copy(originalFilepath, editFilePath, true);
+                var testEntities = new TestEntities(ms);
+                testEntities.EditAttributes(ms);
+                testEntities.VerifyEditedAttribute(ms);
 
-            this.testEntities.EditAttributes(editFilePath, this.Log);
-            this.testEntities.VerifyEditedAttribute(editFilePath, this.Log);
+                testEntities.DeleteTimelineStyle(ms);
+                testEntities.VerifyDeletedTimelineStyle(ms);
 
-            System.IO.File.Copy(editFilePath, deleteTimelineStyleFilePath, true);
-
-            this.testEntities.DeleteTimelineStyle(deleteTimelineStyleFilePath, this.Log);
-            this.testEntities.VerifyDeletedTimelineStyle(deleteTimelineStyleFilePath, this.Log);
-
-            System.IO.File.Copy(deleteTimelineStyleFilePath, addTimelineStyleFilePath, true);
-
-            this.testEntities.AddTimelineStyle(addTimelineStyleFilePath, this.Log);
-            this.testEntities.VerifyAddedTimelineStyle(addTimelineStyleFilePath, this.Log);
+                testEntities.AddTimelineStyle(ms);
+                testEntities.VerifyAddedTimelineStyle(ms);
+            }
         }
     }
 }
