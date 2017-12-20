349873be0e7fca8493f0971c0b5b3d4e8704eeda
Taylor Southwick <tasou@microsoft.com>
2017-12-20 00:25:04 -0800

    Fix namespace

--- DocumentFormat.OpenXml/Validation/Schema/Restrictions/HexBinaryRestriction.cs
+++ DocumentFormat.OpenXml/Validation/Schema/Restrictions/HexBinaryRestriction.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using DocumentFormat.OpenXml.Validation;
 using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 
