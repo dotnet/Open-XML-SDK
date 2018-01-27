bda5e94cfaf76cc40b8c2a55e9a2332a96ce799a
Taylor Southwick <twsouthwick@outlook.com>
2018-01-26 16:03:09 -0800

    Store exceptions when rethrowing

--- src/DocumentFormat.OpenXml/Packaging/OpenXmlPartContainer.cs
+++ src/DocumentFormat.OpenXml/Packaging/OpenXmlPartContainer.cs
@@ -1378,13 +1378,13 @@ namespace DocumentFormat.OpenXml.Packaging
             {
                 InitPart(part, contentType ?? part.ContentType, id);
             }
-            catch (ArgumentOutOfRangeException)
+            catch (ArgumentOutOfRangeException e)
             {
-                throw new OpenXmlPackageException(ExceptionMessages.AddedPartIsNotAllowed);
+                throw new OpenXmlPackageException(e, ExceptionMessages.AddedPartIsNotAllowed);
             }
-            catch (InvalidOperationException)
+            catch (InvalidOperationException e)
             {
-                throw new OpenXmlPackageException(ExceptionMessages.OnlyOnePartAllowed);
+                throw new OpenXmlPackageException(e, ExceptionMessages.OnlyOnePartAllowed);
             }
 
             return part;
