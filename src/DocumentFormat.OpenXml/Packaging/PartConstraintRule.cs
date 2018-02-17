ef86e22446f11039672fe48a8ac621c1339964f9
Taylor Southwick <twsouthwick@outlook.com>
2018-02-17 07:46:02 -0800

    Add tests for OpenXmlPart types

--- src/DocumentFormat.OpenXml/Packaging/PartConstraintRule.cs
+++ src/DocumentFormat.OpenXml/Packaging/PartConstraintRule.cs
@@ -1,57 +1,94 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+
 namespace DocumentFormat.OpenXml.Packaging
 {
-    /// <summary>
-    /// Internal class for part constraint rule
-    /// </summary>
-    internal class PartConstraintRule
+    internal sealed class PartConstraintRule
     {
         /// <summary>
-        /// The class name for the relationship type.
-        /// Used in validation event handler.
+        /// Gets the class name for the relationship type.
         /// </summary>
-        internal string PartClassName;
+        public string PartClassName { get; }
 
         /// <summary>
-        /// Content type of the part. null for non-fixed content type.
-        /// Purpose: WorksheetCommentsPart / SlideCommentsPart / CommentsPart have same relatiohship type but different content type.
+        /// Gets the content type of the part. Some types with fixed content types have
+        /// same relationship type but different content type.
         /// </summary>
-        internal string PartContentType;
+        /// <remarks>This value is null for non-fixed content type.</remarks>
+        public string PartContentType { get; }
 
         /// <summary>
-        /// true if the min occurs > 0, (be required).
+        /// Gets a value indicating whether the min occurs > 0, (i.e. is required).
         /// </summary>
-        internal bool MinOccursIsNonZero;
+        public bool MinOccursIsNonZero { get; }
 
         /// <summary>
-        /// true is max occurs > 1, (is multiple).
+        /// Gets a value indicating whether max occurs > 1, (i.e. has multiple occurances).
         /// </summary>
-        internal bool MaxOccursGreatThanOne;
+        public bool MaxOccursGreatThanOne { get; }
 
         /// <summary>
-        /// The file format version (Office2007 / Office2010) information.
+        /// Gets the file format version information.
         /// </summary>
-        internal FileFormatVersions FileFormat;
+        public FileFormatVersions FileFormat { get; }
 
         /// <summary>
-        /// Initialize a instance of PartConstraintRule class.
+        /// Initialize an instance of <see cref="PartConstraintRule"/>
         /// </summary>
         /// <param name="partClassName">The class name of the part.</param>
         /// <param name="partContentType">The content type of the part.</param>
         /// <param name="minOccursIsNonZero">The MinOccursIsNonZero data.</param>
         /// <param name="maxOccursGreatThanOne">The MaxOccursGreatThanOne data.</param>
         /// <param name="fileFormat">The file format version information.</param>
-        internal PartConstraintRule(string partClassName, string partContentType,
-                                    bool minOccursIsNonZero, bool maxOccursGreatThanOne,
-                                    FileFormatVersions fileFormat)
+        public PartConstraintRule(
+            string partClassName,
+            string partContentType,
+            bool minOccursIsNonZero,
+            bool maxOccursGreatThanOne,
+            FileFormatVersions fileFormat)
         {
-            this.PartClassName = partClassName;
-            this.PartContentType = partContentType;
-            this.MinOccursIsNonZero = minOccursIsNonZero;
-            this.MaxOccursGreatThanOne = maxOccursGreatThanOne;
-            this.FileFormat = fileFormat;
+            PartClassName = partClassName;
+            PartContentType = partContentType;
+            MinOccursIsNonZero = minOccursIsNonZero;
+            MaxOccursGreatThanOne = maxOccursGreatThanOne;
+            FileFormat = fileFormat;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj is PartConstraintRule other)
+            {
+                return string.Equals(PartClassName, other.PartClassName, StringComparison.Ordinal)
+                    && string.Equals(PartContentType, other.PartContentType, StringComparison.Ordinal)
+                    && MinOccursIsNonZero == other.MinOccursIsNonZero
+                    && MaxOccursGreatThanOne == other.MaxOccursGreatThanOne
+                    && FileFormat == other.FileFormat;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            const int HashMultiplier = 31;
+
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * HashMultiplier + StringComparer.Ordinal.GetHashCode(PartClassName);
+                hash = hash * HashMultiplier + StringComparer.Ordinal.GetHashCode(PartContentType);
+                hash = hash * HashMultiplier + MinOccursIsNonZero.GetHashCode();
+                hash = hash * HashMultiplier + MaxOccursGreatThanOne.GetHashCode();
+                hash = hash * HashMultiplier + FileFormat.GetHashCode();
+                return hash;
+            }
         }
     }
 }
