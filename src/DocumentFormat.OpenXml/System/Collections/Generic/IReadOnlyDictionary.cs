d0874fedfa5ebf403a7caf870fe0e021aa554942
Taylor Southwick <twsouthwick@outlook.com>
2018-03-07 12:03:20 -0800

    Add custom ireadonlydictionary

---
+++ src/DocumentFormat.OpenXml/System/Collections/Generic/IReadOnlyDictionary.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace System.Collections.Generic
+{
+    internal interface IReadOnlyDictionary<TKey, TValue> : IReadOnlyCollection<KeyValuePair<TKey, TValue>>
+    {
+        TValue this[TKey key] { get; }
+
+        IEnumerable<TKey> Keys { get; }
+
+        IEnumerable<TValue> Values { get; }
+
+        bool ContainsKey(TKey key);
+
+        bool TryGetValue(TKey key, out TValue value);
+    }
+}
