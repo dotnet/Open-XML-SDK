1013d0eaabaf6bbeb433ccde4fc1c422b3022301
Taylor Southwick <tasou@microsoft.com>
2018-04-05 16:20:02 -0700

    Enforce single class per file (#466)

---
+++ test/DocumentFormat.OpenXml.Tests/ConformanceTest/CommentEx/AppendCommentExIDs.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace DocumentFormat.OpenXml.Tests.CommentEx
+{
+    /// <summary>
+    /// Element CommentEx in paraId attribute
+    /// </summary>
+    public static class AppendCommentExIDs
+    {
+        public static string AppendCommentID1 { get { return "11111111"; } }
+
+        public static string AppendCommentID2 { get { return "22222222"; } }
+    }
+}
---
+++ test/DocumentFormat.OpenXml.Tests/ConformanceTest/CommentEx/CommentBodyStrings.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace DocumentFormat.OpenXml.Tests.CommentEx
+{
+    /// <summary>
+    /// Word document body text
+    /// </summary>
+    public static class CommentBodyStrings
+    {
+        public static string Comment1 { get { return "Test1"; } }
+
+        public static string Comment2 { get { return "Test2"; } }
+    }
+}
