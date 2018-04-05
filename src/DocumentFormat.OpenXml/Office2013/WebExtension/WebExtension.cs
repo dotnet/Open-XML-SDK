97e7650fd144023d178046cd5e680bb3a4c5f5ad
Taylor Southwick <tasou@microsoft.com>
2018-04-04 21:05:33 -0700

    Remove schema attr

--- src/DocumentFormat.OpenXml/Office2013/WebExtension/WebExtension.cs
+++ src/DocumentFormat.OpenXml/Office2013/WebExtension/WebExtension.cs
@@ -11,7 +11,6 @@ namespace DocumentFormat.OpenXml.Office2013.WebExtension
         /// Gets or sets the <see cref="Frozen"/> value. It is only available for backwards compatibility
         /// until the next major version change. The <see cref="Frozen"/> property should be used instead.
         /// </summary>
-        [SchemaAttr(0, "frozen")]
         [Obsolete("Please use the property 'Frozen' as this property will be removed in a future version")]
         public BooleanValue Fronzen
         {
