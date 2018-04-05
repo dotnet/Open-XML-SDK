7b685ed4b2592d10f60695d79b634003be6a5718
Taylor Southwick <tasou@microsoft.com>
2018-04-04 21:03:46 -0700

    Change summary

--- src/DocumentFormat.OpenXml/Office2013/WebExtension/WebExtension.cs
+++ src/DocumentFormat.OpenXml/Office2013/WebExtension/WebExtension.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#pragma warning disable SA1623
-
 using System;
 
 namespace DocumentFormat.OpenXml.Office2013.WebExtension
@@ -10,10 +8,8 @@ namespace DocumentFormat.OpenXml.Office2013.WebExtension
     public partial class WebExtension
     {
         /// <summary>
-        /// <para> frozen.</para>
-        /// <para>Represents the following attribute in the schema: frozen </para>
-        /// This property will be removed in a future version and should be replaced with <see cref="Frozen"/>. It is only available
-        /// for backwards compatibility until the next major version change.
+        /// Gets or sets the <see cref="Frozen"/> value. It is only available for backwards compatibility
+        /// until the next major version change. The <see cref="Frozen"/> property should be used instead.
         /// </summary>
         [SchemaAttr(0, "frozen")]
         [Obsolete("Please use the property 'Frozen' as this property will be removed in a future version")]
