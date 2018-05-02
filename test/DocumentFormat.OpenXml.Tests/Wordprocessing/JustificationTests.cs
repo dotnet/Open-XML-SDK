dca432aa8242a199b96ddfffc14bbc7ed22f3903
Taylor Southwick <tasou@microsoft.com>
2018-05-01 23:46:35 -0700

    Add unit tests for strict translation

---
+++ test/DocumentFormat.OpenXml.Tests/Wordprocessing/JustificationTests.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Xunit;
+
+namespace DocumentFormat.OpenXml.Wordprocessing
+{
+    public class JustificationTests
+    {
+        [InlineData("val", "start", "left")]
+        [InlineData("val", "end", "right")]
+        [InlineData("val", "other", "other")]
+        [Theory]
+        public void JustificationAttributeTranslationTest(string localName, string value, string newValue)
+        {
+            var element = new Justification();
+            var xml = $"<w:tab xmlns:w=\"{element.NamespaceUri}\" w:{localName}=\"{value}\" />";
+
+            element.OuterXml = xml;
+
+            Assert.True(element.HasAttributes);
+            var attribute = Assert.Single(element.GetAttributes());
+
+            Assert.Equal(localName, attribute.LocalName);
+            Assert.Equal(newValue, attribute.Value);
+        }
+    }
+}
---
+++ test/DocumentFormat.OpenXml.Tests/Wordprocessing/TextDirectionTests.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Xunit;
+
+namespace DocumentFormat.OpenXml.Wordprocessing
+{
+    public class TextDirectionTests
+    {
+        [InlineData("val", "lr", "btLr")]
+        [InlineData("val", "tb", "lrTb")]
+        [InlineData("val", "tbV", "lrTbV")]
+        [InlineData("val", "lrV", "tbLrV")]
+        [InlineData("val", "rl", "tbRl")]
+        [InlineData("val", "rlV", "tbRlV")]
+        [Theory]
+        public void TextDirectionTranslationTest(string localName, string value, string newValue)
+        {
+            var element = new TextDirection();
+            var xml = $"<w:tab xmlns:w=\"{element.NamespaceUri}\" w:{localName}=\"{value}\" />";
+
+            element.OuterXml = xml;
+
+            Assert.True(element.HasAttributes);
+            var attribute = Assert.Single(element.GetAttributes());
+
+            Assert.Equal(localName, attribute.LocalName);
+            Assert.Equal(newValue, attribute.Value);
+        }
+    }
+}
