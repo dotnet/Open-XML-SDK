3772fce601d4b42f639d0adcedd86c0a58afd77d
Taylor Southwick <twsouthwick@outlook.com>
2018-05-30 12:40:19 -0700

    Add FormattableString

---
+++ src/DocumentFormat.OpenXml/System/Runtime/CompilerServices/FormattableStringFactory.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace System.Runtime.CompilerServices
+{
+	internal static class FormattableStringFactory
+	{
+		public static FormattableString Create(string format, params object[] arguments)
+		{
+			return new FormattableString(format, arguments);
+		}
+	}
+}
