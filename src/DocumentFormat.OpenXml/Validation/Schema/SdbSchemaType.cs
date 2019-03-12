53ce8b7de0e4e470cb72b4a856cb433082739d19
Taylor Southwick <tasou@microsoft.com>
2019-03-12 11:40:56 -0700

    Do not use readonly auto-properties as they break with .NET Native

--- src/DocumentFormat.OpenXml/Validation/Schema/SdbSchemaType.cs
+++ src/DocumentFormat.OpenXml/Validation/Schema/SdbSchemaType.cs
@@ -5,15 +5,23 @@ using System.Runtime.InteropServices;
 
 namespace DocumentFormat.OpenXml.Validation.Schema
 {
+    /// <remarks>
+    /// We cannot use auto-properties here due to failure in .NET Native to compile them. For further details, https://github.com/OfficeDev/Open-XML-SDK/issues/567.
+    /// </remarks>
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     internal readonly struct SdbSchemaType
     {
+        private readonly ushort _particleIndex;
+        private readonly ushort _simpleTypeIndex;
+        private readonly ushort _attributesCount;
+        private readonly ushort _startIndexOfAttributes;
+
         public SdbSchemaType(ushort particleIndex, ushort simpleTypeIndex, ushort attributesCount, ushort startIndexOfAttributes)
         {
-            ParticleIndex = particleIndex;
-            SimpleTypeIndex = simpleTypeIndex;
-            AttributesCount = attributesCount;
-            StartIndexOfAttributes = startIndexOfAttributes;
+            _particleIndex = particleIndex;
+            _simpleTypeIndex = simpleTypeIndex;
+            _attributesCount = attributesCount;
+            _startIndexOfAttributes = startIndexOfAttributes;
         }
 
         /// <summary>
@@ -21,23 +29,23 @@ namespace DocumentFormat.OpenXml.Validation.Schema
         /// Will be "SdbData.InvalidId" if the schema type is NOT composite type.
         /// </summary>
         /// <remarks>This field can be eliminated.</remarks>
-        public ushort ParticleIndex { get; }
+        public ushort ParticleIndex => _particleIndex;
 
         /// <summary>
         /// Gets the index of the simple data in the SdbSimpleTypeRestriction data array.
         /// Will be "SdbData.InvalidId" if the schema type is NOT simple content only.
         /// </summary>
-        public ushort SimpleTypeIndex { get; }
+        public ushort SimpleTypeIndex => _simpleTypeIndex;
 
         /// <summary>
         /// Gets the count of attributes.
         /// </summary>
-        public ushort AttributesCount { get; }
+        public ushort AttributesCount => _attributesCount;
 
         /// <summary>
         /// Gets the index of the first attribute data in the SdbAttributeConstraint data array.
         /// </summary>
-        public ushort StartIndexOfAttributes { get; }
+        public ushort StartIndexOfAttributes => _startIndexOfAttributes;
 
         /// <summary>
         /// Gets a value indicating whether the schema type is a schema type which contains particles.
