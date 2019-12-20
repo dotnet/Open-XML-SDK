3f371befbe1d5bd9acc87ca091454990284be34a
Taylor Southwick <twsouthwick@outlook.com>
2019-12-19 21:19:41 -0800

    Remove namespace

--- src/DocumentFormat.OpenXml/Framework/Validation/StringValidatorAttribute.cs
+++ src/DocumentFormat.OpenXml/Framework/Validation/StringValidatorAttribute.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Threading;
-using System.Transactions;
 using System.Xml;
 
 namespace DocumentFormat.OpenXml.Framework
