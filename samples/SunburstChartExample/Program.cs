a947a4d1a6c5485ef4acbcca52b7f994a9feb085
Tom Jebo <tomjebo@live.com>
2020-03-09 11:30:20 -0700

    more cleanup

--- samples/SunburstChartExample/Program.cs
+++ samples/SunburstChartExample/Program.cs
@@ -1,11 +1,11 @@
 ﻿using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Presentation;
+
 using A = DocumentFormat.OpenXml.Drawing;
 using C = DocumentFormat.OpenXml.Drawing.Charts;
 using C16 = DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
 using Cs = DocumentFormat.OpenXml.Office2013.Drawing.ChartStyle;
-using D = DocumentFormat.OpenXml.Drawing;
 using P = DocumentFormat.OpenXml.Presentation;
 using P14 = DocumentFormat.OpenXml.Office2010.PowerPoint;
 
@@ -1579,7 +1579,7 @@ namespace CreatePresentationDocument
                 new A.BodyProperties(),
                 new A.ListStyle(),
                 new A.Paragraph())))),
-            new P.ColorMap() { Background1 = D.ColorSchemeIndexValues.Light1, Text1 = D.ColorSchemeIndexValues.Dark1, Background2 = D.ColorSchemeIndexValues.Light2, Text2 = D.ColorSchemeIndexValues.Dark2, Accent1 = D.ColorSchemeIndexValues.Accent1, Accent2 = D.ColorSchemeIndexValues.Accent2, Accent3 = D.ColorSchemeIndexValues.Accent3, Accent4 = D.ColorSchemeIndexValues.Accent4, Accent5 = D.ColorSchemeIndexValues.Accent5, Accent6 = D.ColorSchemeIndexValues.Accent6, Hyperlink = D.ColorSchemeIndexValues.Hyperlink, FollowedHyperlink = D.ColorSchemeIndexValues.FollowedHyperlink },
+            new P.ColorMap() { Background1 = A.ColorSchemeIndexValues.Light1, Text1 = A.ColorSchemeIndexValues.Dark1, Background2 = A.ColorSchemeIndexValues.Light2, Text2 = A.ColorSchemeIndexValues.Dark2, Accent1 = A.ColorSchemeIndexValues.Accent1, Accent2 = A.ColorSchemeIndexValues.Accent2, Accent3 = A.ColorSchemeIndexValues.Accent3, Accent4 = A.ColorSchemeIndexValues.Accent4, Accent5 = A.ColorSchemeIndexValues.Accent5, Accent6 = A.ColorSchemeIndexValues.Accent6, Hyperlink = A.ColorSchemeIndexValues.Hyperlink, FollowedHyperlink = A.ColorSchemeIndexValues.FollowedHyperlink },
             new SlideLayoutIdList(new SlideLayoutId() { Id = (UInt32Value)2147483649U, RelationshipId = "rId1" }),
             new TextStyles(new TitleStyle(), new BodyStyle(), new OtherStyle()));
             slideMasterPart1.SlideMaster = slideMaster;
@@ -1590,155 +1590,155 @@ namespace CreatePresentationDocument
         private static ThemePart CreateTheme(SlideMasterPart slideMasterPart1)
         {
             ThemePart themePart1 = slideMasterPart1.AddNewPart<ThemePart>("rId5");
-            D.Theme theme1 = new D.Theme() { Name = "Office Theme" };
+            A.Theme theme1 = new A.Theme() { Name = "Office Theme" };
 
-            D.ThemeElements themeElements1 = new D.ThemeElements(
-            new D.ColorScheme(
-              new D.Dark1Color(new D.SystemColor() { Val = D.SystemColorValues.WindowText, LastColor = "000000" }),
-              new D.Light1Color(new D.SystemColor() { Val = D.SystemColorValues.Window, LastColor = "FFFFFF" }),
-              new D.Dark2Color(new D.RgbColorModelHex() { Val = "1F497D" }),
-              new D.Light2Color(new D.RgbColorModelHex() { Val = "EEECE1" }),
-              new D.Accent1Color(new D.RgbColorModelHex() { Val = "4F81BD" }),
-              new D.Accent2Color(new D.RgbColorModelHex() { Val = "C0504D" }),
-              new D.Accent3Color(new D.RgbColorModelHex() { Val = "9BBB59" }),
-              new D.Accent4Color(new D.RgbColorModelHex() { Val = "8064A2" }),
-              new D.Accent5Color(new D.RgbColorModelHex() { Val = "4BACC6" }),
-              new D.Accent6Color(new D.RgbColorModelHex() { Val = "F79646" }),
-              new D.Hyperlink(new D.RgbColorModelHex() { Val = "0000FF" }),
-              new D.FollowedHyperlinkColor(new D.RgbColorModelHex() { Val = "800080" }))
+            A.ThemeElements themeElements1 = new A.ThemeElements(
+            new A.ColorScheme(
+              new A.Dark1Color(new A.SystemColor() { Val = A.SystemColorValues.WindowText, LastColor = "000000" }),
+              new A.Light1Color(new A.SystemColor() { Val = A.SystemColorValues.Window, LastColor = "FFFFFF" }),
+              new A.Dark2Color(new A.RgbColorModelHex() { Val = "1F497D" }),
+              new A.Light2Color(new A.RgbColorModelHex() { Val = "EEECE1" }),
+              new A.Accent1Color(new A.RgbColorModelHex() { Val = "4F81BD" }),
+              new A.Accent2Color(new A.RgbColorModelHex() { Val = "C0504D" }),
+              new A.Accent3Color(new A.RgbColorModelHex() { Val = "9BBB59" }),
+              new A.Accent4Color(new A.RgbColorModelHex() { Val = "8064A2" }),
+              new A.Accent5Color(new A.RgbColorModelHex() { Val = "4BACC6" }),
+              new A.Accent6Color(new A.RgbColorModelHex() { Val = "F79646" }),
+              new A.Hyperlink(new A.RgbColorModelHex() { Val = "0000FF" }),
+              new A.FollowedHyperlinkColor(new A.RgbColorModelHex() { Val = "800080" }))
             { Name = "Office" },
-              new D.FontScheme(
-              new D.MajorFont(
-              new D.LatinFont() { Typeface = "Calibri" },
-              new D.EastAsianFont() { Typeface = "" },
-              new D.ComplexScriptFont() { Typeface = "" }),
-              new D.MinorFont(
-              new D.LatinFont() { Typeface = "Calibri" },
-              new D.EastAsianFont() { Typeface = "" },
-              new D.ComplexScriptFont() { Typeface = "" }))
+              new A.FontScheme(
+              new A.MajorFont(
+              new A.LatinFont() { Typeface = "Calibri" },
+              new A.EastAsianFont() { Typeface = "" },
+              new A.ComplexScriptFont() { Typeface = "" }),
+              new A.MinorFont(
+              new A.LatinFont() { Typeface = "Calibri" },
+              new A.EastAsianFont() { Typeface = "" },
+              new A.ComplexScriptFont() { Typeface = "" }))
               { Name = "Office" },
-              new D.FormatScheme(
-              new D.FillStyleList(
-              new D.SolidFill(new D.SchemeColor() { Val = D.SchemeColorValues.PhColor }),
-              new D.GradientFill(
-                new D.GradientStopList(
-                new D.GradientStop(new D.SchemeColor(new D.Tint() { Val = 50000 },
-                  new D.SaturationModulation() { Val = 300000 })
-                { Val = D.SchemeColorValues.PhColor })
+              new A.FormatScheme(
+              new A.FillStyleList(
+              new A.SolidFill(new A.SchemeColor() { Val = A.SchemeColorValues.PhColor }),
+              new A.GradientFill(
+                new A.GradientStopList(
+                new A.GradientStop(new A.SchemeColor(new A.Tint() { Val = 50000 },
+                  new A.SaturationModulation() { Val = 300000 })
+                { Val = A.SchemeColorValues.PhColor })
                 { Position = 0 },
-                new D.GradientStop(new D.SchemeColor(new D.Tint() { Val = 37000 },
-                 new D.SaturationModulation() { Val = 300000 })
-                { Val = D.SchemeColorValues.PhColor })
+                new A.GradientStop(new A.SchemeColor(new A.Tint() { Val = 37000 },
+                 new A.SaturationModulation() { Val = 300000 })
+                { Val = A.SchemeColorValues.PhColor })
                 { Position = 35000 },
-                new D.GradientStop(new D.SchemeColor(new D.Tint() { Val = 15000 },
-                 new D.SaturationModulation() { Val = 350000 })
-                { Val = D.SchemeColorValues.PhColor })
+                new A.GradientStop(new A.SchemeColor(new A.Tint() { Val = 15000 },
+                 new A.SaturationModulation() { Val = 350000 })
+                { Val = A.SchemeColorValues.PhColor })
                 { Position = 100000 }
                 ),
-                new D.LinearGradientFill() { Angle = 16200000, Scaled = true }),
-              new D.NoFill(),
-              new D.PatternFill(),
-              new D.GroupFill()),
-              new D.LineStyleList(
-              new D.Outline(
-                new D.SolidFill(
-                new D.SchemeColor(
-                  new D.Shade() { Val = 95000 },
-                  new D.SaturationModulation() { Val = 105000 })
-                { Val = D.SchemeColorValues.PhColor }),
-                new D.PresetDash() { Val = D.PresetLineDashValues.Solid })
+                new A.LinearGradientFill() { Angle = 16200000, Scaled = true }),
+              new A.NoFill(),
+              new A.PatternFill(),
+              new A.GroupFill()),
+              new A.LineStyleList(
+              new A.Outline(
+                new A.SolidFill(
+                new A.SchemeColor(
+                  new A.Shade() { Val = 95000 },
+                  new A.SaturationModulation() { Val = 105000 })
+                { Val = A.SchemeColorValues.PhColor }),
+                new A.PresetDash() { Val = A.PresetLineDashValues.Solid })
               {
                   Width = 9525,
-                  CapType = D.LineCapValues.Flat,
-                  CompoundLineType = D.CompoundLineValues.Single,
-                  Alignment = D.PenAlignmentValues.Center
+                  CapType = A.LineCapValues.Flat,
+                  CompoundLineType = A.CompoundLineValues.Single,
+                  Alignment = A.PenAlignmentValues.Center
               },
-              new D.Outline(
-                new D.SolidFill(
-                new D.SchemeColor(
-                  new D.Shade() { Val = 95000 },
-                  new D.SaturationModulation() { Val = 105000 })
-                { Val = D.SchemeColorValues.PhColor }),
-                new D.PresetDash() { Val = D.PresetLineDashValues.Solid })
+              new A.Outline(
+                new A.SolidFill(
+                new A.SchemeColor(
+                  new A.Shade() { Val = 95000 },
+                  new A.SaturationModulation() { Val = 105000 })
+                { Val = A.SchemeColorValues.PhColor }),
+                new A.PresetDash() { Val = A.PresetLineDashValues.Solid })
               {
                   Width = 9525,
-                  CapType = D.LineCapValues.Flat,
-                  CompoundLineType = D.CompoundLineValues.Single,
-                  Alignment = D.PenAlignmentValues.Center
+                  CapType = A.LineCapValues.Flat,
+                  CompoundLineType = A.CompoundLineValues.Single,
+                  Alignment = A.PenAlignmentValues.Center
               },
-              new D.Outline(
-                new D.SolidFill(
-                new D.SchemeColor(
-                  new D.Shade() { Val = 95000 },
-                  new D.SaturationModulation() { Val = 105000 })
-                { Val = D.SchemeColorValues.PhColor }),
-                new D.PresetDash() { Val = D.PresetLineDashValues.Solid })
+              new A.Outline(
+                new A.SolidFill(
+                new A.SchemeColor(
+                  new A.Shade() { Val = 95000 },
+                  new A.SaturationModulation() { Val = 105000 })
+                { Val = A.SchemeColorValues.PhColor }),
+                new A.PresetDash() { Val = A.PresetLineDashValues.Solid })
               {
                   Width = 9525,
-                  CapType = D.LineCapValues.Flat,
-                  CompoundLineType = D.CompoundLineValues.Single,
-                  Alignment = D.PenAlignmentValues.Center
+                  CapType = A.LineCapValues.Flat,
+                  CompoundLineType = A.CompoundLineValues.Single,
+                  Alignment = A.PenAlignmentValues.Center
               }),
-              new D.EffectStyleList(
-              new D.EffectStyle(
-                new D.EffectList(
-                new D.OuterShadow(
-                  new D.RgbColorModelHex(
-                  new D.Alpha() { Val = 38000 })
+              new A.EffectStyleList(
+              new A.EffectStyle(
+                new A.EffectList(
+                new A.OuterShadow(
+                  new A.RgbColorModelHex(
+                  new A.Alpha() { Val = 38000 })
                   { Val = "000000" })
                 { BlurRadius = 40000L, Distance = 20000L, Direction = 5400000, RotateWithShape = false })),
-              new D.EffectStyle(
-                new D.EffectList(
-                new D.OuterShadow(
-                  new D.RgbColorModelHex(
-                  new D.Alpha() { Val = 38000 })
+              new A.EffectStyle(
+                new A.EffectList(
+                new A.OuterShadow(
+                  new A.RgbColorModelHex(
+                  new A.Alpha() { Val = 38000 })
                   { Val = "000000" })
                 { BlurRadius = 40000L, Distance = 20000L, Direction = 5400000, RotateWithShape = false })),
-              new D.EffectStyle(
-                new D.EffectList(
-                new D.OuterShadow(
-                  new D.RgbColorModelHex(
-                  new D.Alpha() { Val = 38000 })
+              new A.EffectStyle(
+                new A.EffectList(
+                new A.OuterShadow(
+                  new A.RgbColorModelHex(
+                  new A.Alpha() { Val = 38000 })
                   { Val = "000000" })
                 { BlurRadius = 40000L, Distance = 20000L, Direction = 5400000, RotateWithShape = false }))),
-              new D.BackgroundFillStyleList(
-              new D.SolidFill(new D.SchemeColor() { Val = D.SchemeColorValues.PhColor }),
-              new D.GradientFill(
-                new D.GradientStopList(
-                new D.GradientStop(
-                  new D.SchemeColor(new D.Tint() { Val = 50000 },
-                    new D.SaturationModulation() { Val = 300000 })
-                  { Val = D.SchemeColorValues.PhColor })
+              new A.BackgroundFillStyleList(
+              new A.SolidFill(new A.SchemeColor() { Val = A.SchemeColorValues.PhColor }),
+              new A.GradientFill(
+                new A.GradientStopList(
+                new A.GradientStop(
+                  new A.SchemeColor(new A.Tint() { Val = 50000 },
+                    new A.SaturationModulation() { Val = 300000 })
+                  { Val = A.SchemeColorValues.PhColor })
                 { Position = 0 },
-                new D.GradientStop(
-                  new D.SchemeColor(new D.Tint() { Val = 50000 },
-                    new D.SaturationModulation() { Val = 300000 })
-                  { Val = D.SchemeColorValues.PhColor })
+                new A.GradientStop(
+                  new A.SchemeColor(new A.Tint() { Val = 50000 },
+                    new A.SaturationModulation() { Val = 300000 })
+                  { Val = A.SchemeColorValues.PhColor })
                 { Position = 0 },
-                new D.GradientStop(
-                  new D.SchemeColor(new D.Tint() { Val = 50000 },
-                    new D.SaturationModulation() { Val = 300000 })
-                  { Val = D.SchemeColorValues.PhColor })
+                new A.GradientStop(
+                  new A.SchemeColor(new A.Tint() { Val = 50000 },
+                    new A.SaturationModulation() { Val = 300000 })
+                  { Val = A.SchemeColorValues.PhColor })
                 { Position = 0 }),
-                new D.LinearGradientFill() { Angle = 16200000, Scaled = true }),
-              new D.GradientFill(
-                new D.GradientStopList(
-                new D.GradientStop(
-                  new D.SchemeColor(new D.Tint() { Val = 50000 },
-                    new D.SaturationModulation() { Val = 300000 })
-                  { Val = D.SchemeColorValues.PhColor })
+                new A.LinearGradientFill() { Angle = 16200000, Scaled = true }),
+              new A.GradientFill(
+                new A.GradientStopList(
+                new A.GradientStop(
+                  new A.SchemeColor(new A.Tint() { Val = 50000 },
+                    new A.SaturationModulation() { Val = 300000 })
+                  { Val = A.SchemeColorValues.PhColor })
                 { Position = 0 },
-                new D.GradientStop(
-                  new D.SchemeColor(new D.Tint() { Val = 50000 },
-                    new D.SaturationModulation() { Val = 300000 })
-                  { Val = D.SchemeColorValues.PhColor })
+                new A.GradientStop(
+                  new A.SchemeColor(new A.Tint() { Val = 50000 },
+                    new A.SaturationModulation() { Val = 300000 })
+                  { Val = A.SchemeColorValues.PhColor })
                 { Position = 0 }),
-                new D.LinearGradientFill() { Angle = 16200000, Scaled = true })))
+                new A.LinearGradientFill() { Angle = 16200000, Scaled = true })))
               { Name = "Office" });
 
             theme1.Append(themeElements1);
-            theme1.Append(new D.ObjectDefaults());
-            theme1.Append(new D.ExtraColorSchemeList());
+            theme1.Append(new A.ObjectDefaults());
+            theme1.Append(new A.ExtraColorSchemeList());
 
             themePart1.Theme = theme1;
             return themePart1;
