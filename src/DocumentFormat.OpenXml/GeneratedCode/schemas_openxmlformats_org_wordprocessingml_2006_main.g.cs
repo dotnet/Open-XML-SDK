e473881a27be803bb9dcc91197d23802746a5993
Taylor Southwick <twsouthwick@outlook.com>
2020-03-23 15:26:59 -0700

    Additional compat settings

--- src/DocumentFormat.OpenXml/GeneratedCode/schemas_openxmlformats_org_wordprocessingml_2006_main.g.cs
+++ src/DocumentFormat.OpenXml/GeneratedCode/schemas_openxmlformats_org_wordprocessingml_2006_main.g.cs
@@ -55591,5 +55591,17 @@ namespace DocumentFormat.OpenXml.Wordprocessing
         ///</summary>
         [EnumString("useWord2013TrackBottomHyphenation")]
         UseWord2013TrackBottomHyphenation,
+        ///<summary>
+        ///allowHyphenationAtTrackBottom.
+        ///<para>When the item is serialized out as xml, its value is "allowHyphenationAtTrackBottom".</para>
+        ///</summary>
+        [EnumString("allowHyphenationAtTrackBottom")]
+        AllowHyphenationAtTrackBottom,
+        ///<summary>
+        ///allowTextAfterFloatingTableBreak.
+        ///<para>When the item is serialized out as xml, its value is "allowTextAfterFloatingTableBreak".</para>
+        ///</summary>
+        [EnumString("allowTextAfterFloatingTableBreak")]
+        AllowTextAfterFloatingTableBreak,
     }
 }
