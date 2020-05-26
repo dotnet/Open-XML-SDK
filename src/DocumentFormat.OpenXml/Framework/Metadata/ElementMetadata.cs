8e9bd36bbd0dc9b54c07768cbe1265044b1af1e9
Taylor Southwick <twsouthwick@outlook.com>
2020-05-26 09:54:45 -0700

    Move attribute collection

--- src/DocumentFormat.OpenXml/Framework/Metadata/ElementMetadata.cs
+++ src/DocumentFormat.OpenXml/Framework/Metadata/ElementMetadata.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 
@@ -24,8 +23,8 @@ namespace DocumentFormat.OpenXml.Framework.Metadata
         {
             var data = _lookup.GetOrAdd(type, t =>
             {
-                var helper = typeof(CreatorHelper<>).MakeGenericType(t);
-                var builder = (IElementMetadataBuilder)Activator.CreateInstance(helper);
+                var helper = typeof(BuilderProvider<>).MakeGenericType(t);
+                var builder = (IBuilderProvider)Activator.CreateInstance(helper);
 
                 return builder.Create();
             });
@@ -38,7 +37,7 @@ namespace DocumentFormat.OpenXml.Framework.Metadata
         {
             var data = _lookup.GetOrAdd(typeof(TElement), t =>
             {
-                var builder = new CreatorHelper<TElement>();
+                var builder = new BuilderProvider<TElement>();
 
                 return builder.Create();
             });
@@ -46,7 +45,7 @@ namespace DocumentFormat.OpenXml.Framework.Metadata
             return new ElementMetadata(data);
         }
 
-        private class CreatorHelper<T> : IElementMetadataBuilder
+        private class BuilderProvider<T> : IBuilderProvider
             where T : OpenXmlElement, new()
         {
             public ReadOnlyArray<AttributeMetadata> Create()
@@ -61,7 +60,7 @@ namespace DocumentFormat.OpenXml.Framework.Metadata
             }
         }
 
-        private interface IElementMetadataBuilder
+        private interface IBuilderProvider
         {
             ReadOnlyArray<AttributeMetadata> Create();
         }
@@ -72,121 +71,5 @@ namespace DocumentFormat.OpenXml.Framework.Metadata
         }
 
         public AttributeCollection Attributes { get; }
-    }
-
-    internal readonly struct AttributeCollection : IEnumerable<AttributeCollection.AttributeEntry>
-    {
-        private readonly OpenXmlSimpleType[] _data;
-        private readonly ReadOnlyArray<AttributeMetadata> _attributes;
-
-        public AttributeCollection(ReadOnlyArray<AttributeMetadata> tags)
-        {
-            _attributes = tags;
-
-            if (tags.Length == 0)
-            {
-                _data = Array.Empty<OpenXmlSimpleType>();
-            }
-            else
-            {
-                _data = new OpenXmlSimpleType[tags.Length];
-            }
-        }
-
-        public bool IsEmpty => _data is null;
-
-        public bool Any() => Length > 0;
-
-        public ref OpenXmlSimpleType GetProperty(string propertyName)
-        {
-            for (var i = 0; i < _attributes.Length; i++)
-            {
-                var property = _attributes[i];
-
-                if (property.PropertyName.Equals(propertyName, StringComparison.Ordinal))
-                {
-                    return ref _data[i];
-                }
-            }
-
-            throw new InvalidOperationException();
-        }
-
-        public AttributeEntry this[int index] => new AttributeEntry(this, index);
-
-        public AttributeEntry this[string namespaceUri, string tagName] => this[GetIndex(namespaceUri, tagName)];
-
-        public int Length => _attributes.Length;
-
-        private int GetIndex(string namespaceUri, string tagName)
-        {
-            if (!string.IsNullOrEmpty(tagName) && NamespaceIdMap.TryGetNamespaceId(namespaceUri, out var nsId))
-            {
-                for (var i = 0; i < _attributes.Length; i++)
-                {
-                    var tag = _attributes[i];
-
-                    if (tag.Name.Equals(tagName, StringComparison.Ordinal) && tag.NamespaceId == nsId)
-                    {
-                        return i;
-                    }
-                }
-            }
-
-            return -1;
-        }
-
-        public Enumerator GetEnumerator() => new Enumerator(in this);
-
-        IEnumerator<AttributeEntry> IEnumerable<AttributeEntry>.GetEnumerator() => GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-        public struct Enumerator : IEnumerator<AttributeEntry>
-        {
-            private readonly AttributeCollection _collection;
-            private int _index;
-
-            public Enumerator(in AttributeCollection collection)
-            {
-                _collection = collection;
-                _index = -1;
-            }
-
-            public AttributeEntry Current => _collection[_index];
-
-            object IEnumerator.Current => Current;
-
-            public bool MoveNext() => ++_index < _collection.Length;
-
-            void IDisposable.Dispose()
-            {
-            }
-
-            void IEnumerator.Reset() => throw new NotImplementedException();
-        }
-
-        public readonly struct AttributeEntry
-        {
-            private readonly AttributeCollection _collection;
-
-            private readonly int _index;
-
-            public AttributeEntry(in AttributeCollection collection, int index)
-            {
-                _collection = collection;
-                _index = index;
-            }
-
-            public bool IsNil => _index == -1;
-
-            public ref readonly AttributeMetadata Property => ref _collection._attributes[_index];
-
-            public OpenXmlSimpleType Value => _collection._data[_index];
-
-            public void SetValue(OpenXmlSimpleType value) => _collection._data[_index] = value;
-
-            public bool HasValue => Value != null;
-        }
     }
 }
