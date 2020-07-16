3016ad097c188f979b4f7c4c86dbe9695d6ba9a9
lindexi <lindexi_gd@163.com>
2020-07-16 11:57:38 +0800

    Add Benchmark for create metadata

---
+++ test/DocumentFormat.OpenXml.Benchmarks/ElementMetadataTests.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using DocumentFormat.OpenXml.Framework.Metadata;
+
+namespace DocumentFormat.OpenXml.Benchmarks
+{
+    public class ElementMetadataTests
+    {
+        [GlobalSetup]
+        public void Setup()
+        {
+            _element = new AlternateContent();
+        }
+
+        [Benchmark]
+        public void CreateWithInstance()
+        {
+            _ = ElementMetadata.Create(_element);
+        }
+
+        [Benchmark]
+        public void CreateWithGeneric()
+        {
+            _ = ElementMetadata.Create<AlternateContent>();
+        }
+
+        private OpenXmlElement _element;
+    }
+}
