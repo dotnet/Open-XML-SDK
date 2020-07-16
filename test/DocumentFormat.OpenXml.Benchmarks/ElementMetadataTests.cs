03fcb99a384d9eb7f0c9b17ab6ee287ec1e514aa
lindexi <lindexi_gd@163.com>
2020-07-16 12:00:36 +0800

    Add file head

--- test/DocumentFormat.OpenXml.Benchmarks/ElementMetadataTests.cs
+++ test/DocumentFormat.OpenXml.Benchmarks/ElementMetadataTests.cs
@@ -1,4 +1,7 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using BenchmarkDotNet.Attributes;
 using DocumentFormat.OpenXml.Framework.Metadata;
 
 namespace DocumentFormat.OpenXml.Benchmarks
