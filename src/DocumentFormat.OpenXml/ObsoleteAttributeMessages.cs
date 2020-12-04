5b41859b0741c4a261d597cfe9919687520e38df
Taylor Southwick <twsouthwick@outlook.com>
2020-12-04 11:05:06 -0800

    Create OpenXmlSchema
    Use OpenXmlSchema to track schema information rather than SchemaAttrAttribute as it should now be deprecated

--- src/DocumentFormat.OpenXml/ObsoleteAttributeMessages.cs
+++ src/DocumentFormat.OpenXml/ObsoleteAttributeMessages.cs
@@ -5,7 +5,6 @@ namespace DocumentFormat.OpenXml
 {
     internal static class ObsoleteAttributeMessages
     {
-        // This message is used in ObsoleteAttribute().
         internal const string ObsoleteV1ValidationFunctionality = "This functionality is obsolete and will be removed from future version release. Please see OpenXmlValidator class for supported validation functionality.";
     }
 }
