8fd3fecc79606e2816fcafcb07f05005de0bd0b8
Taylor Southwick <twsouthwick@outlook.com>
2021-01-11 10:17:20 -0800

    All properties to be set to null
    A previous change had added a null check which inadvertantly caused properties to not be able to be set to null. This fixes that so that children properties can again be set to null.

--- src/DocumentFormat.OpenXml/OpenXmlCompositeElement.cs
+++ src/DocumentFormat.OpenXml/OpenXmlCompositeElement.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using DocumentFormat.OpenXml.Framework;
-using DocumentFormat.OpenXml.Validation.Schema;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -222,7 +221,7 @@ namespace DocumentFormat.OpenXml
                 return false;
             }
 
-            var wasAdded = SetElement(newChild);
+            var wasAdded = Metadata.Particle.Set(this, newChild, newChild?.GetType());
 
             if (throwOnError && !wasAdded)
             {
@@ -789,7 +788,8 @@ namespace DocumentFormat.OpenXml
         private protected TElement GetElement<TElement>()
             where TElement : OpenXmlElement => Metadata.Particle.Get<TElement>(this);
 
-        private protected bool SetElement(OpenXmlElement value)
+        private protected bool SetElement<TElement>(TElement value)
+            where TElement : OpenXmlElement
             => Metadata.Particle.Set(this, value);
 
         private void AddANode(OpenXmlElement node)
