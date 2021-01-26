cdf376c6b915fb4441d7bcff2c3e1feccce375d9
Taylor Southwick <twsouthwick@outlook.com>
2021-01-25 22:48:19 -0800

    more

--- src/DocumentFormat.OpenXml/System/Runtime/CompilerServices/IsExternalInit.cs
+++ src/DocumentFormat.OpenXml/System/Runtime/CompilerServices/IsExternalInit.cs
@@ -1,9 +1,16 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.ComponentModel;
+ 
 namespace System.Runtime.CompilerServices
 {
-    internal static class IsExternalInit
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class IsExternalInit
     {
     }
 }
