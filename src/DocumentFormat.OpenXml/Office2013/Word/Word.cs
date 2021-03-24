e9ac703f5c1d024b894b55a048b10f5c45cb965a
Tom Jebo <tomjebo@live.com>
2021-03-24 14:32:43 -0700

    added the obsolete attribute to the Person.Contact property to be removed in a later version

---
+++ src/DocumentFormat.OpenXml/Office2013/Word/Word.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentFormat.OpenXml.Office2013.Word
+{
+    public partial class Person : OpenXmlCompositeElement
+    {
+        /// <summary>
+        /// Gets or sets the <see cref="Contact"/> value. It is only available for backwards compatibility
+        /// until the next major version change. Please discontinue using this property.
+        /// </summary>
+        [Obsolete("This property will be removed in a future version")]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public StringValue? Contact
+        {
+            get => GetAttribute<StringValue>();
+            set => SetAttribute(value);
+        }
+    }
+}
