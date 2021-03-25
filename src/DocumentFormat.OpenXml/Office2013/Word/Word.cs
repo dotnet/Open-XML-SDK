38a1298340b0820dcc18c86bd7c8aa42345cbf0e
Tom Jebo <tomjebo@live.com>
2021-03-25 13:41:39 -0700

    removed unused usings

--- src/DocumentFormat.OpenXml/Office2013/Word/Word.cs
+++ src/DocumentFormat.OpenXml/Office2013/Word/Word.cs
@@ -2,11 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DocumentFormat.OpenXml.Office2013.Word
 {
