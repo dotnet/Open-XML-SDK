a9f83c76c512accddeb52cb95ba049b38a1371d6
Tom Jebo <tomjebo@live.com>
2021-04-14 10:15:02 -0700

    another revert

--- src/DocumentFormat.OpenXml/Validation/ValidationResources.Designer.cs
+++ src/DocumentFormat.OpenXml/Validation/ValidationResources.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace DocumentFormat.OpenXml.Validation {
     using System;
+    using System.Reflection;
     
     
     /// <summary>
@@ -39,7 +40,7 @@ namespace DocumentFormat.OpenXml.Validation {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DocumentFormat.OpenXml.Validation.ValidationResources", typeof(ValidationResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DocumentFormat.OpenXml.Validation.ValidationResources", typeof(ValidationResources).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
