b590315bc5b55dde244284c4a6a24f7cf9720072
Tom Jebo <tomjebo@live.com>
2021-04-22 11:06:09 -0700

    headers missing and usings not sorted

--- test/DocumentFormat.OpenXml.Tests/Spreadsheet/CellValueTests.cs
+++ test/DocumentFormat.OpenXml.Tests/Spreadsheet/CellValueTests.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using DocumentFormat.OpenXml.Tests.Common;
 using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Tests.Common;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
