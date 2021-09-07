f2674699fcfb538ff0df7256d626401667fe27b8
Taylor Southwick <tasou@microsoft.com>
2021-09-06 23:12:01 -0700

    Fix up null

--- src/DocumentFormat.OpenXml/Framework/ServiceExtensions.cs
+++ src/DocumentFormat.OpenXml/Framework/ServiceExtensions.cs
@@ -1,13 +1,21 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using DocumentFormat.OpenXml.Framework.Metadata;
 using System;
 
 namespace DocumentFormat.OpenXml.Framework
 {
     internal static class ServiceExtensions
     {
-        public static T GetRequiredService<T>(this IServiceProvider services) => (T)services.GetService(typeof(T));
+        public static T GetRequiredService<T>(this IServiceProvider services)
+            where T : class
+        {
+            if (services.GetService(typeof(T)) is T t)
+            {
+                return t;
+            }
+
+            throw new InvalidOperationException(SR.Format(ExceptionMessages.UnknownService, typeof(T).FullName));
+        }
     }
 }
--- src/DocumentFormat.OpenXml/Resources/ExceptionMessages.Designer.cs
+++ src/DocumentFormat.OpenXml/Resources/ExceptionMessages.Designer.cs
@@ -10,7 +10,6 @@
 
 namespace DocumentFormat.OpenXml {
     using System;
-    using System.Reflection;
     
     
     /// <summary>
@@ -20,7 +19,7 @@ namespace DocumentFormat.OpenXml {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class ExceptionMessages {
@@ -40,7 +39,7 @@ namespace DocumentFormat.OpenXml {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DocumentFormat.OpenXml.Resources.ExceptionMessages", typeof(ExceptionMessages).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DocumentFormat.OpenXml.Resources.ExceptionMessages", typeof(ExceptionMessages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -898,6 +897,15 @@ namespace DocumentFormat.OpenXml {
         internal static string UnknownPackage {
             get {
                 return ResourceManager.GetString("UnknownPackage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Service {0} is not available..
+        /// </summary>
+        internal static string UnknownService {
+            get {
+                return ResourceManager.GetString("UnknownService", resourceCulture);
             }
         }
         
