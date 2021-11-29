fafa71efc0d8f00c90ed9fba7937d7289d973996
Jaime Still <jpstill85@gmail.com>
2021-11-29 18:40:16 -0500

    created ImagePartExtensions in support of #1081

---
+++ src/DocumentFormat.OpenXml/Packaging/ImagePartTypeExtensions.cs
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+
+namespace DocumentFormat.OpenXml.Packaging
+{
+    /// <summary>
+    /// Provides extensions to working with ImagePart and ImagePartType
+    /// </summary>
+    public static class ImagePartExtensions
+    {
+        /// <summary>
+        /// Extends an overloaded AddImagePart method that
+        /// handles the process of determining the ImagePartType
+        /// for you based on a provided file extension.
+        /// </summary>
+        /// <param name="main">The source MainDocumentPart that is adding the ImagePart.</param>
+        /// <param name="extension">The file extension of the image.</param>
+        public static ImagePart AddImagePart(this MainDocumentPart main, string extension) =>
+            main.AddImagePart(GetImagePartType(extension));
+
+        static ImagePartType GetImagePartType(string extension)
+            => extension.ToLower() switch
+            {
+                ".bmp" => ImagePartType.Bmp,
+                ".emf" => ImagePartType.Emf,
+                ".ico" => ImagePartType.Icon,
+                ".jpg" => ImagePartType.Jpeg,
+                ".jpeg" => ImagePartType.Jpeg,
+                ".pcx" => ImagePartType.Pcx,
+                ".png" => ImagePartType.Png,
+                ".svg" => ImagePartType.Svg,
+                ".tiff" => ImagePartType.Tiff,
+                ".wmf" => ImagePartType.Wmf,
+                _ => throw new NotSupportedException($"{extension} is not supported")
+            };
+    }
+}
