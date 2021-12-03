bdd8f14570f1e7291273140358360267a2ccdff6
Michael Bowen <mikee.bowen@gmail.com>
2021-12-03 15:29:25 -0800

    put header at the top of the file

--- samples/SVGExample/Program.cs
+++ samples/SVGExample/Program.cs
@@ -1,4 +1,7 @@
-﻿using DocumentFormat.OpenXml;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Drawing;
 using DocumentFormat.OpenXml.Office2019.Drawing.SVG;
 using DocumentFormat.OpenXml.Packaging;
@@ -7,9 +10,6 @@ using Svg;
 using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
