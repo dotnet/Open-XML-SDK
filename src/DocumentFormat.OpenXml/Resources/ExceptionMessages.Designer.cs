1232577393a82ec88991853dd33ef3c0ad640d7b
Taylor Southwick <tasou@microsoft.com>
2021-12-13 14:42:32 -0800

    fix reflection using

--- src/DocumentFormat.OpenXml/Resources/ExceptionMessages.Designer.cs
+++ src/DocumentFormat.OpenXml/Resources/ExceptionMessages.Designer.cs
@@ -10,8 +10,9 @@
 
 namespace DocumentFormat.OpenXml {
     using System;
-    
-    
+    using System.Reflection;
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -39,7 +40,7 @@ namespace DocumentFormat.OpenXml {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DocumentFormat.OpenXml.Resources.ExceptionMessages", typeof(ExceptionMessages).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DocumentFormat.OpenXml.Resources.ExceptionMessages", typeof(ExceptionMessages).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
