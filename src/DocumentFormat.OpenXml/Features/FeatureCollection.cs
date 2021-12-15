eac86b6a95bca0088d912002d05229ccbb2e09a7
Taylor Southwick <tasou@microsoft.com>
2021-12-14 17:21:17 -0800

    revert

--- src/DocumentFormat.OpenXml/Features/FeatureCollection.cs
+++ src/DocumentFormat.OpenXml/Features/FeatureCollection.cs
@@ -60,11 +60,7 @@ namespace DocumentFormat.OpenXml.Features
         /// <inheritdoc/>
         public TFeature? Get<TFeature>()
         {
-            if (GetFeature<TFeature>() is TFeature existing)
-            {
-                return existing;
-            }
-            else if (_features is not null && _features.TryGetValue(typeof(TFeature), out var feature) && feature is TFeature t)
+            if (_features is not null && _features.TryGetValue(typeof(TFeature), out var feature) && feature is TFeature t)
             {
                 return t;
             }
@@ -75,13 +71,6 @@ namespace DocumentFormat.OpenXml.Features
 
             return default;
         }
-
-        /// <summary>
-        /// Gets a feature from derived classes.
-        /// </summary>
-        /// <typeparam name="TFeature">Feature type.</typeparam>
-        /// <returns>Feature if found.</returns>
-        protected virtual TFeature? GetFeature<TFeature>() => default;
 
         /// <inheritdoc/>
         public void Set<TFeature>(TFeature? instance)
