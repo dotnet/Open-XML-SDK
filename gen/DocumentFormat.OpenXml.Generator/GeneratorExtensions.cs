26713b2940274fc80321a50596b15b533bc96078
Taylor Southwick <tasou@microsoft.com>
2022-02-18 10:33:38 -0800

    WIP: Enable generators

--- gen/DocumentFormat.OpenXml.Generator/GeneratorExtensions.cs
+++ gen/DocumentFormat.OpenXml.Generator/GeneratorExtensions.cs
@@ -1,17 +1,60 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using DocumentFormat.OpenXml.Generator.Models;
 using Microsoft.CodeAnalysis;
 using System.Collections.Immutable;
+
+using IncrementalOpenXmlTextValuesProvider = Microsoft.CodeAnalysis.IncrementalValuesProvider<(Microsoft.CodeAnalysis.AdditionalText Text, string Type)>;
 
 namespace DocumentFormat.OpenXml.Generator;
 
 public static class GeneratorExtensions
 {
-    public static IncrementalValueProvider<ImmutableArray<AdditionalText>> GetOpenXmlDataFiles(this IncrementalGeneratorInitializationContext context, string name)
+    public static IncrementalOpenXmlTextValuesProvider GetOpenXmlDataFiles(this IncrementalGeneratorInitializationContext context)
         => context.AdditionalTextsProvider
                 .Combine(context.AnalyzerConfigOptionsProvider)
-                .Where(args => args.Right.GetOptions(args.Left).TryGetValue("build_metadata.AdditionalFiles.OpenXml", out var result) && string.Equals(name, result, StringComparison.Ordinal))
-                .Select((args, _) => args.Left)
-                .Collect();
+                .Select((args, token) =>
+                {
+                    if (args.Right.GetOptions(args.Left).TryGetValue("build_metadata.AdditionalFiles.OpenXml", out var result))
+                    {
+                        return (args.Left, result);
+                    }
+
+                    return default;
+                })
+                .Where(args => args != default);
+
+    public static IncrementalValuesProvider<AdditionalText> IsOpenXmlType(this IncrementalOpenXmlTextValuesProvider types, string type)
+        => types.Where(t => string.Equals(t.Type, type, StringComparison.Ordinal)).Select((t, _) => t.Text);
+
+    public static IncrementalValuesProvider<T> Deserialize<T>(this IncrementalValuesProvider<AdditionalText> texts)
+        => texts.Select((t, token) => OpenXmlGeneratorContext.Deserialize<T>(t.GetText(token)?.ToString())).Where(t => t is not null)!;
+
+    public static IncrementalValueProvider<ImmutableArray<NamespaceInfo>> GetKnownNamespaces(this IncrementalOpenXmlTextValuesProvider types)
+        => types
+            .IsOpenXmlType("Namespace")
+            .Deserialize<NamespaceInfo[]>()
+            .SelectMany((n, _) => n ?? Enumerable.Empty<NamespaceInfo>())
+            .Collect();
+
+    public static IncrementalValueProvider<ImmutableArray<SchemaNamespace>> GetNamespaceTypes(this IncrementalOpenXmlTextValuesProvider texts)
+        => texts
+            .IsOpenXmlType("Schema")
+            .Deserialize<SchemaNamespace>()
+            .Collect();
+
+    public static IncrementalValueProvider<OpenXmlGeneratorContext> GetOpenXmlGeneratorContext(this IncrementalGeneratorInitializationContext context)
+    {
+        var openXmlFiles = context.GetOpenXmlDataFiles();
+        var namespaces = openXmlFiles.GetKnownNamespaces();
+        var namespaceTypes = openXmlFiles.GetNamespaceTypes();
+
+        return namespaces.Combine(namespaceTypes)
+            .Select((arg, token) => new OpenXmlGeneratorContext
+            {
+                KnownNamespaces = arg.Left,
+                Namespaces = arg.Right,
+            });
+    }
 }
--- gen/DocumentFormat.OpenXml.Generator/NamespaceGeneration/NamespaceGenerator.cs
+++ gen/DocumentFormat.OpenXml.Generator/NamespaceGeneration/NamespaceGenerator.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using DocumentFormat.OpenXml.Generator.Models;
 using Microsoft.CodeAnalysis;
 
 namespace DocumentFormat.OpenXml.Generator.NamespaceGeneration;
@@ -12,7 +13,7 @@ public class NamespaceGenerator : IIncrementalGenerator
 
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        var namespaces = context.GetOpenXmlDataFiles("Namespace");
+        var namespaces = context.GetOpenXmlDataFiles().GetKnownNamespaces();
 
         context.RegisterSourceOutput(namespaces.Combine(context.AnalyzerConfigOptionsProvider), static (context, data) =>
         {
@@ -20,9 +21,7 @@ public class NamespaceGenerator : IIncrementalGenerator
             {
                 if (data.Right.GlobalOptions.TryGetValue("build_property.DocumentFormat_OpenXml_GeneratorNamespaceLookup", out var generatorSwitch) && bool.TryParse(generatorSwitch, out var result) && result)
                 {
-                    var ooxmlContext = new OpenXmlGeneratorContext().LoadNamespaces(data.Left, context.CancellationToken);
-
-                    context.AddSource("Namespaces", ooxmlContext.KnownNamespaces.Generate());
+                    context.AddSource("Namespaces", data.Left.Generate());
                 }
             }
             catch (Exception)
