5071f719c711b7e090d02bb7ddf07faa0c3e98f2
Taylor Southwick <tasou@microsoft.com>
2022-03-01 16:10:06 -0800

    Enable part generator

--- gen/DocumentFormat.OpenXml.Generator/GeneratorExtensions.cs
+++ gen/DocumentFormat.OpenXml.Generator/GeneratorExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using DocumentFormat.OpenXml.Generator.Models;
+using DocumentFormat.OpenXml.Generator.Schematron;
 using Microsoft.CodeAnalysis;
 using System.Collections.Immutable;
 
@@ -11,6 +12,79 @@ namespace DocumentFormat.OpenXml.Generator;
 
 public static class GeneratorExtensions
 {
+    // NOTE: These must align with the metadata defined in SourceGenerator.targets
+    private const string AdditionalFileNamespace = "Namespace";
+    private const string AdditionalFilePart = "Part";
+    private const string AdditionalFileSchema = "Schema";
+    private const string AdditionalFileSchematron = "Schematron";
+    private const string AdditionalFileTypedSchema = "TypedSchema";
+    private const string AdditionalFileTypedNamespace = "TypedNamespace";
+
+    public static IncrementalValueProvider<ImmutableArray<NamespaceInfo>> GetKnownNamespaces(this IncrementalOpenXmlTextValuesProvider types)
+        => types
+            .IsOpenXmlType(AdditionalFileNamespace)
+            .Deserialize<NamespaceInfo[]>()
+            .SelectMany((n, _) => n ?? Enumerable.Empty<NamespaceInfo>())
+            .Collect();
+
+    public static IncrementalValueProvider<ImmutableArray<Part>> GetParts(this IncrementalOpenXmlTextValuesProvider types)
+        => types
+            .IsOpenXmlType(AdditionalFilePart)
+            .Deserialize<Part>()
+            .Collect();
+
+    public static IncrementalValueProvider<ImmutableArray<SchemaNamespace>> GetNamespaceTypes(this IncrementalOpenXmlTextValuesProvider texts)
+        => texts
+            .IsOpenXmlType(AdditionalFileSchema)
+            .Deserialize<SchemaNamespace>()
+            .Collect();
+
+    public static IncrementalValueProvider<ImmutableArray<SchematronEntry>> GetSchematrons(this IncrementalOpenXmlTextValuesProvider texts)
+        => texts
+            .IsOpenXmlType(AdditionalFileSchematron)
+            .Deserialize<SchematronEntry[]>()
+            .SelectMany((s, _) => s ?? Enumerable.Empty<SchematronEntry>())
+            .Collect();
+
+    public static IncrementalValueProvider<ImmutableArray<StronglyTypedElement>> GetStronglyTypedSchemas(this IncrementalOpenXmlTextValuesProvider texts)
+        => texts
+            .IsOpenXmlType(AdditionalFileTypedSchema)
+            .Deserialize<StronglyTypedElement[]>()
+            .SelectMany((s, _) => s ?? Enumerable.Empty<StronglyTypedElement>())
+            .Collect();
+
+    public static IncrementalValueProvider<ImmutableArray<StronglyTypedNamespace>> GetStronglyTypedNamespace(this IncrementalOpenXmlTextValuesProvider texts)
+        => texts
+            .IsOpenXmlType(AdditionalFileTypedNamespace)
+            .Deserialize<StronglyTypedNamespace[]>()
+            .SelectMany((s, _) => s ?? Enumerable.Empty<StronglyTypedNamespace>())
+            .Collect();
+
+    public static IncrementalValueProvider<OpenXmlGeneratorContext> GetOpenXmlGeneratorContext(this IncrementalGeneratorInitializationContext context)
+    {
+        var openXmlFiles = context.GetOpenXmlDataFiles();
+        var namespaces = openXmlFiles.GetKnownNamespaces();
+        var namespaceTypes = openXmlFiles.GetNamespaceTypes();
+        var parts = openXmlFiles.GetParts();
+        var schematrons = openXmlFiles.GetSchematrons();
+        var stronglyTypedSchema = openXmlFiles.GetStronglyTypedSchemas();
+        var stronglyTypedNamespace = openXmlFiles.GetStronglyTypedNamespace();
+
+        return namespaces.Combine(namespaceTypes).Combine(parts).Combine(schematrons).Combine(stronglyTypedSchema).Combine(stronglyTypedNamespace)
+            .Select((arg, token) => new OpenXmlGeneratorContext
+            {
+                KnownNamespaces = arg.Left.Left.Left.Left.Left,
+                Namespaces = arg.Left.Left.Left.Left.Right,
+                Parts = arg.Left.Left.Left.Right,
+                Schematrons = arg.Left.Left.Right,
+                TypedClasses = arg.Left.Right,
+                TypedNamespaces = arg.Right,
+            });
+    }
+
+    public static IncrementalValueProvider<OpenXmlGeneratorServices> GetOpenXmlServices(this IncrementalValueProvider<OpenXmlGeneratorContext> context)
+        => context.Select((context, _) => new OpenXmlGeneratorServices(context));
+
     public static IncrementalOpenXmlTextValuesProvider GetOpenXmlDataFiles(this IncrementalGeneratorInitializationContext context)
         => context.AdditionalTextsProvider
                 .Combine(context.AnalyzerConfigOptionsProvider)
@@ -25,36 +99,9 @@ public static class GeneratorExtensions
                 })
                 .Where(args => args != default);
 
-    public static IncrementalValuesProvider<AdditionalText> IsOpenXmlType(this IncrementalOpenXmlTextValuesProvider types, string type)
+    private static IncrementalValuesProvider<AdditionalText> IsOpenXmlType(this IncrementalOpenXmlTextValuesProvider types, string type)
         => types.Where(t => string.Equals(t.Type, type, StringComparison.Ordinal)).Select((t, _) => t.Text);
 
-    public static IncrementalValuesProvider<T> Deserialize<T>(this IncrementalValuesProvider<AdditionalText> texts)
+    private static IncrementalValuesProvider<T> Deserialize<T>(this IncrementalValuesProvider<AdditionalText> texts)
         => texts.Select((t, token) => OpenXmlGeneratorContext.Deserialize<T>(t.GetText(token)?.ToString())).Where(t => t is not null)!;
-
-    public static IncrementalValueProvider<ImmutableArray<NamespaceInfo>> GetKnownNamespaces(this IncrementalOpenXmlTextValuesProvider types)
-        => types
-            .IsOpenXmlType("Namespace")
-            .Deserialize<NamespaceInfo[]>()
-            .SelectMany((n, _) => n ?? Enumerable.Empty<NamespaceInfo>())
-            .Collect();
-
-    public static IncrementalValueProvider<ImmutableArray<SchemaNamespace>> GetNamespaceTypes(this IncrementalOpenXmlTextValuesProvider texts)
-        => texts
-            .IsOpenXmlType("Schema")
-            .Deserialize<SchemaNamespace>()
-            .Collect();
-
-    public static IncrementalValueProvider<OpenXmlGeneratorContext> GetOpenXmlGeneratorContext(this IncrementalGeneratorInitializationContext context)
-    {
-        var openXmlFiles = context.GetOpenXmlDataFiles();
-        var namespaces = openXmlFiles.GetKnownNamespaces();
-        var namespaceTypes = openXmlFiles.GetNamespaceTypes();
-
-        return namespaces.Combine(namespaceTypes)
-            .Select((arg, token) => new OpenXmlGeneratorContext
-            {
-                KnownNamespaces = arg.Left,
-                Namespaces = arg.Right,
-            });
-    }
 }
---
+++ gen/DocumentFormat.OpenXml.Generator/PartGenerator.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using DocumentFormat.OpenXml.Generator.Editor;
+using DocumentFormat.OpenXml.Generator.Generators.Parts;
+using Microsoft.CodeAnalysis;
+using System.CodeDom.Compiler;
+using System.Text;
+
+namespace DocumentFormat.OpenXml.Generator;
+
+[Generator]
+public class PartGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var openXml = context.GetOpenXmlGeneratorContext().GetOpenXmlServices();
+
+        context.RegisterSourceOutput(openXml, (context, openXml) =>
+        {
+            var sb = new StringBuilder();
+            var sw = new StringWriter(sb);
+            var writer = new IndentedTextWriter(sw);
+
+            foreach (var part in openXml.Context.Parts)
+            {
+                sb.Clear();
+
+                writer.WriteFileHeader();
+                writer.WriteLine();
+                writer.WritePart(openXml, part);
+
+                context.AddSource(part.Name, sb.ToString());
+            }
+        });
+    }
+}
