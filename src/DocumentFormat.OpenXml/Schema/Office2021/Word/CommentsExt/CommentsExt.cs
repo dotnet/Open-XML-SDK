0363a7e97c188dc35ef055e130b2ba4a73213565
Michael Bowen <mikee.bowen@gmail.com>
2022-06-23 12:50:10 -0700

    add getter and setter shims so ExtensionList is valid

--- src/DocumentFormat.OpenXml/Schema/Office2021/Word/CommentsExt/CommentsExt.cs
+++ src/DocumentFormat.OpenXml/Schema/Office2021/Word/CommentsExt/CommentsExt.cs
@@ -3,6 +3,8 @@
 
 using System;
 using System.ComponentModel;
+using System.Reflection;
+using System.Xml;
 
 namespace DocumentFormat.OpenXml.Office2021.Word.CommentsExt
 {
@@ -16,8 +18,86 @@ namespace DocumentFormat.OpenXml.Office2021.Word.CommentsExt
         [EditorBrowsable(EditorBrowsableState.Never)]
         public ExtensionList? ExtensionList
         {
-            get => CommentExtensibleExtensionList != null ? (ExtensionList?)(OpenXmlCompositeElement)CommentExtensibleExtensionList : null;
-            set => CommentExtensibleExtensionList = value != null ? (CommentExtensibleExtensionList?)(OpenXmlCompositeElement)value : null;
+            get
+            {
+                ExtensionList? extLst = null;
+
+                if (CommentExtensibleExtensionList != null)
+                {
+                    extLst = new ExtensionList(CommentExtensibleExtensionList.OuterXml);
+
+                    foreach (PropertyInfo prop in CommentExtensibleExtensionList.GetType().GetProperties())
+                    {
+                        PropertyInfo? exProp = extLst.GetType().GetProperty(prop.Name);
+
+                        if (exProp != null && exProp.PropertyType == prop.PropertyType)
+                        {
+                            if (exProp.GetSetMethod() != null)
+                            {
+                                PropertyInfo? valProp = CommentExtensibleExtensionList.GetType().GetProperty(prop.Name);
+
+                                if (valProp != null && valProp.CanRead)
+                                {
+                                    exProp.SetValue(extLst, valProp.GetValue(CommentExtensibleExtensionList));
+                                }
+                                else if (valProp != null && !valProp.CanRead && valProp.Name == "InnerXml")
+                                {
+                                    XmlDocument xmlDocument = new XmlDocument();
+                                    xmlDocument.LoadXml(extLst.OuterXml);
+                                    XmlElement? root = xmlDocument.DocumentElement;
+
+                                    if (root != null && root.InnerXml != null)
+                                    {
+                                        extLst.InnerXml = root.InnerXml;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return extLst;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    if (CommentExtensibleExtensionList == null)
+                    {
+                        CommentExtensibleExtensionList = new CommentExtensibleExtensionList();
+                    }
+
+                    foreach (PropertyInfo prop in value.GetType().GetProperties())
+                    {
+                        PropertyInfo? cmtExtListProp = CommentExtensibleExtensionList?.GetType().GetProperty(prop.Name);
+
+                        if (cmtExtListProp != null && cmtExtListProp.PropertyType == prop.PropertyType)
+                        {
+                            if (cmtExtListProp.GetSetMethod() != null)
+                            {
+                                PropertyInfo? valProp = value.GetType()?.GetProperty(prop.Name);
+
+                                if (valProp != null && valProp.CanRead)
+                                {
+                                    cmtExtListProp.SetValue(CommentExtensibleExtensionList, valProp.GetValue(value));
+                                }
+                                else if (valProp != null && !valProp.CanRead && valProp.Name == "InnerXml")
+                                {
+                                    XmlDocument xmlDocument = new XmlDocument();
+                                    xmlDocument.LoadXml(value.OuterXml);
+                                    XmlElement? root = xmlDocument.DocumentElement;
+
+                                    if (root != null && root.InnerXml != null && CommentExtensibleExtensionList != null)
+                                    {
+                                        CommentExtensibleExtensionList.InnerXml = root.InnerXml;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
