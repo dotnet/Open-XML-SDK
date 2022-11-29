84d50c512bf190b1eb178a8f419e12f71dedcefe
Taylor Southwick <tasou@microsoft.com>
2022-11-29 12:43:47 -0800

    WIP

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Validation;
+
+var file = @"C:\Users\tasou\OneDrive - Microsoft\Documents\MainLiveDoc.pptx";
+
+
+using var doc = PresentationDocument.Open(file, false);
+
+var parts = doc.GetAllParts();
+
+var validator = new OpenXmlValidator();
+
+var result = validator.Validate(doc);
+
+foreach (var part in result)
+{
+    Console.WriteLine(part.Description);
+}
