bead4ee232c33e7f1275dc808bd56d6f05975c66
Taylor Southwick <tasou@microsoft.com>
2023-01-27 18:29:49 -0800

    reorder

--- src/DocumentFormat.OpenXml.Framework/Features/FeatureEventArgs.cs
+++ src/DocumentFormat.OpenXml.Framework/Features/FeatureEventArgs.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using DocumentFormat.OpenXml.Framework;
+using System;
 
 namespace DocumentFormat.OpenXml.Features
 {
