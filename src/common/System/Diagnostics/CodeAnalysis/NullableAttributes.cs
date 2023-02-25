6cc681053afe5191f626a0c5e4cdbf8d8c6af381
Taylor Southwick <tasou@microsoft.com>
2023-02-24 19:31:59 -0800

    Fix warning

--- src/common/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
+++ src/common/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
@@ -3,6 +3,7 @@
 
 #pragma warning disable SA1402 // File may only contain a single type
 #pragma warning disable SA1649 // File name should match first type name
+#pragma warning disable CA1019 // Add a public read-only property accessor for positional argument member
 
 namespace System.Diagnostics.CodeAnalysis
 {
