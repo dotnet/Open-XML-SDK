fe78a94e6d124a4529649879a13fff7e63a08534
Michael Bowen <mikee.bowen@gmail.com>
2023-03-31 13:52:29 -0700

    add additional tests

--- test/DocumentFormat.OpenXml.Framework.Tests/OpenXmlPartTests.cs
+++ test/DocumentFormat.OpenXml.Framework.Tests/OpenXmlPartTests.cs
@@ -2,11 +2,7 @@
 using DocumentFormat.OpenXml.Packaging;
 using NSubstitute;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace DocumentFormat.OpenXml.Framework.Tests
@@ -20,6 +16,7 @@ namespace DocumentFormat.OpenXml.Framework.Tests
             // Arrange
             const string ContentType = "test/mimetype";
             const string TargetExt = ".jpg";
+            const string FilePath = "/some/path.jpg";
 
             using var stream = new MemoryStream();
 
@@ -33,7 +30,7 @@ namespace DocumentFormat.OpenXml.Framework.Tests
             var partExtensionFeature = Substitute.For<IPartExtensionFeature>();
             var partUriFeature = Substitute.For<IPartUriFeature>();
             var contentTypeFeature = Substitute.For<IContentTypeFeature>();
-            var expectedUri = new Uri("/some/path.jpg", UriKind.Relative);
+            var expectedUri = new Uri(FilePath, UriKind.Relative);
             partUriFeature.CreatePartUri(ContentType, OpenXmlPackage.Uri, string.Empty, string.Empty, TargetExt).Returns(expectedUri);
 
             features.Set(partUriFeature);
@@ -45,7 +42,85 @@ namespace DocumentFormat.OpenXml.Framework.Tests
             part.CreateInternal(package, null, ContentType, TargetExt);
 
             // Assert
-            Assert.Equal("/some/path.jpg", part.Uri.ToString());
+            Assert.Equal(FilePath, part.Uri.ToString());
+        }
+
+        [Fact]
+        public void ExtensionTestKnownContentType()
+        {
+            // Arrange
+            const string ContentType = "image/jpeg";
+            const string TargetExt = ".foo";
+            const string ExpectedExt = ".bar";
+            const string FilePath = "/some/path.bar";
+
+            using var stream = new MemoryStream();
+
+            var package = Substitute.ForPartsOf<OpenXmlPackage>();
+            package.WithStorage(stream, PackageOpenMode.Create);
+
+            var features = new FeatureCollection();
+            var part = Substitute.ForPartsOf<OpenXmlPart>();
+            part.Features.Returns(features);
+            var targetFeature = Substitute.For<ITargetFeature>();
+            var partExtensionFeature = Substitute.For<IPartExtensionFeature>();
+            var partUriFeature = Substitute.For<IPartUriFeature>();
+            var contentTypeFeature = Substitute.For<IContentTypeFeature>();
+            var expectedUri = new Uri(FilePath, UriKind.Relative);
+
+            partExtensionFeature.TryGetExtension(ContentType, out string expectedExt).Returns(callInfo =>
+            {
+                callInfo[1] = ExpectedExt;
+                return true;
+            });
+
+            partUriFeature.CreatePartUri(ContentType, OpenXmlPackage.Uri, string.Empty, string.Empty, ExpectedExt).Returns(expectedUri);
+
+            features.Set(partUriFeature);
+            features.Set(partExtensionFeature);
+            features.Set(targetFeature);
+            features.Set(contentTypeFeature);
+
+            // Act
+            part.CreateInternal(package, null, ContentType, TargetExt);
+
+            // Assert
+            Assert.Equal(FilePath, part.Uri.ToString());
+        }
+
+        [Fact]
+        public void ExtensionTestUndefinedExtension()
+        {
+            // Arrange
+            const string ContentType = "test/mimetype";
+            const string FilePath = "/some/path.jpg";
+
+            using var stream = new MemoryStream();
+
+            var package = Substitute.ForPartsOf<OpenXmlPackage>();
+            package.WithStorage(stream, PackageOpenMode.Create);
+
+            var features = new FeatureCollection();
+            var part = Substitute.ForPartsOf<OpenXmlPart>();
+            part.Features.Returns(features);
+            var targetFeature = Substitute.For<ITargetFeature>();
+            var partExtensionFeature = Substitute.For<IPartExtensionFeature>();
+            var partUriFeature = Substitute.For<IPartUriFeature>();
+            var contentTypeFeature = Substitute.For<IContentTypeFeature>();
+            var expectedUri = new Uri(FilePath, UriKind.Relative);
+
+            partUriFeature.CreatePartUri(ContentType, OpenXmlPackage.Uri, string.Empty, string.Empty, string.Empty).Returns(expectedUri);
+
+            features.Set(partUriFeature);
+            features.Set(partExtensionFeature);
+            features.Set(targetFeature);
+            features.Set(contentTypeFeature);
+
+            // Act
+            part.CreateInternal(package, null, ContentType, string.Empty);
+
+            // Assert
+            Assert.Equal(FilePath, part.Uri.ToString());
         }
     }
 }
