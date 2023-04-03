d0fedbc7f76741f541daff0e067e2457435e21b2
Michael Bowen <mikee.bowen@gmail.com>
2023-04-03 11:24:31 -0700

    add missing file header

--- test/DocumentFormat.OpenXml.Framework.Tests/OpenXmlPartTests.cs
+++ test/DocumentFormat.OpenXml.Framework.Tests/OpenXmlPartTests.cs
@@ -1,4 +1,7 @@
-﻿using DocumentFormat.OpenXml.Features;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using DocumentFormat.OpenXml.Features;
 using DocumentFormat.OpenXml.Packaging;
 using NSubstitute;
 using System;
