8ca26487a4e73ab6e63ea85c10bfa4d62ce46713
Tom Jebo <tomjebo@microsoft.com>
2023-04-05 13:07:31 -0700

    allow AddEmbeddedPackagePart to use the correct extension for contenttype

---
+++ src/DocumentFormat.OpenXml/Packaging/EmbeddedPackagePartType.cs
@@ -0,0 +1,111 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace DocumentFormat.OpenXml.Packaging
+{
+    /// <summary>
+    /// Defines EmbeddedPackagePartType - types of EmbeddedPackagePart.
+    /// </summary>
+    public enum EmbeddedPackagePartType
+    {
+        /// <summary>
+        /// Office Open XML macro-enabled document (.docm).
+        /// </summary>
+        Docm,
+
+        /// <summary>
+        /// Office Open XML document (.docx).
+        /// </summary>
+        Docx,
+
+        /// <summary>
+        /// Office Open XML macro-enabled document template (.dotm).
+        /// </summary>
+        Dotm,
+
+        /// <summary>
+        /// Office Open XML document template (.dotx).
+        /// </summary>
+        Dotx,
+
+        /// <summary>
+        /// Office Open XML macro-enabled presentation template (.potm).
+        /// </summary>
+        Potm,
+
+        /// <summary>
+        /// Office Open XML presentation template (.potx).
+        /// </summary>
+        Potx,
+
+        /// <summary>
+        /// Office Open XML macro-enabled presentation add-in (.ppam).
+        /// </summary>
+        Ppam,
+
+        /// <summary>
+        /// Office Open XML macroe-enabled show (.ppsm).
+        /// </summary>
+        Ppsm,
+
+        /// <summary>
+        /// Office Open XML show (.ppsx).
+        /// </summary>
+        Ppsx,
+
+        /// <summary>
+        /// Office Open XML macro-enabled presentation (.pptm).
+        /// </summary>
+        Pptm,
+
+        /// <summary>
+        /// Office Open XML presentation (.pptx).
+        /// </summary>
+        Pptx,
+
+        /// <summary>
+        /// Office Open XML macro-enabled slide (.sldm).
+        /// </summary>
+        Sldm,
+
+        /// <summary>
+        /// Office Open XML slide (.sldx).
+        /// </summary>
+        Sldx,
+
+        /// <summary>
+        /// Office Open XML office theme (.thmx).
+        /// </summary>
+        Thmx,
+
+        /// <summary>
+        /// Office Open XML macro-enabled add-in (.xlam).
+        /// </summary>
+        Xlam,
+
+        /// <summary>
+        /// Office Open XML binary workbook (.xlsb).
+        /// </summary>
+        Xlsb,
+
+        /// <summary>
+        /// Office Open XML macro-enabled workbook (.xlsm).
+        /// </summary>
+        Xlsm,
+
+        /// <summary>
+        /// Office Open XML workbook (.xlsx).
+        /// </summary>
+        Xlsx,
+
+        /// <summary>
+        /// Office Open XML macro-enabled workbook template (.xltm).
+        /// </summary>
+        Xltm,
+
+        /// <summary>
+        /// Office Open XML workbook template (.xltx).
+        /// </summary>
+        Xltx,
+    }
+}
