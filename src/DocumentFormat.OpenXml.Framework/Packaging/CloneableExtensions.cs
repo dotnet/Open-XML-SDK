19ab69f71596ce911bcca2d74f2b9a656c55ce52
Taylor Southwick <tasou@microsoft.com>
2023-04-25 09:43:47 -0700

    add headers

--- src/DocumentFormat.OpenXml.Framework/Packaging/CloneableExtensions.cs
+++ src/DocumentFormat.OpenXml.Framework/Packaging/CloneableExtensions.cs
@@ -1,4 +1,7 @@
-﻿using DocumentFormat.OpenXml.Features;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using DocumentFormat.OpenXml.Features;
 using System;
 using System.IO;
 using System.IO.Packaging;
