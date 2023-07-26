5544f27d82c18c4ae87a9f23e80c6b50e2e211e6
Michael Bowen <mikee.bowen@gmail.com>
2023-07-26 16:00:48 -0700

    move mime type mapping to separate method

--- src/DocumentFormat.OpenXml.Framework/Packaging/PartExtensionProvider.cs
+++ src/DocumentFormat.OpenXml.Framework/Packaging/PartExtensionProvider.cs
@@ -19,6 +19,35 @@ namespace DocumentFormat.OpenXml.Packaging
         public PartExtensionProvider()
             : base(StringComparer.Ordinal)
         {
+            AddKnownExtensions();
+        }
+
+        /// <summary>
+        /// Check to make sure the content type and part extension is in the provider. If not, they will be added.
+        /// </summary>
+        /// <param name="contentType">The content type</param>
+        /// <param name="extension">Part Extension (".xml") to be used for the part with the specified content type.</param>
+        /// <exception cref="ArgumentNullException">Thrown when either parameter is null.</exception>
+        public void Register(string contentType, string extension)
+        {
+            if (contentType is null)
+            {
+                throw new ArgumentNullException(nameof(contentType));
+            }
+
+            if (extension is null)
+            {
+                throw new ArgumentNullException(nameof(extension));
+            }
+
+            this[contentType] = extension;
+        }
+
+        public bool TryGetExtension(string contentType, [MaybeNullWhen(false)] out string extension)
+            => TryGetValue(contentType, out extension);
+
+        private void AddKnownExtensions()
+        {
             this["image/unknown"] = ".bin";
             this["image/bmp"] = ".bmp";
             this["image/gif"] = ".gif";
@@ -56,29 +85,5 @@ namespace DocumentFormat.OpenXml.Packaging
             this["video/ogg"] = ".ogg";
             this["video/vc1"] = ".wmv";
         }
-
-        /// <summary>
-        /// Check to make sure the content type and part extension is in the provider. If not, they will be added.
-        /// </summary>
-        /// <param name="contentType">The content type</param>
-        /// <param name="extension">Part Extension (".xml") to be used for the part with the specified content type.</param>
-        /// <exception cref="ArgumentNullException">Thrown when either parameter is null.</exception>
-        public void Register(string contentType, string extension)
-        {
-            if (contentType is null)
-            {
-                throw new ArgumentNullException(nameof(contentType));
-            }
-
-            if (extension is null)
-            {
-                throw new ArgumentNullException(nameof(extension));
-            }
-
-            this[contentType] = extension;
-        }
-
-        public bool TryGetExtension(string contentType, [MaybeNullWhen(false)] out string extension)
-            => TryGetValue(contentType, out extension);
     }
 }
