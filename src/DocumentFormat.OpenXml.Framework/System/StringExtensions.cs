ed5d034027699fdf48f8bdeef48584fbbd01263e
Taylor Southwick <tasou@microsoft.com>
2023-11-06 16:47:00 -0800

    handle updates

--- src/DocumentFormat.OpenXml.Framework/System/StringExtensions.cs
+++ src/DocumentFormat.OpenXml.Framework/System/StringExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #if !NET5_0_OR_GREATER
+
 using System;
 
 namespace DocumentFormat.OpenXml;
@@ -12,7 +13,10 @@ internal static class StringExtensions
         => str.IndexOf(value, StringComparison.Ordinal) != -1;
 
     public static bool Contains(this string str, char c)
-        => str.IndexOf(c.ToString()) != -1;
+        => str.IndexOf(c.ToString(), StringComparison.Ordinal) != -1;
+
+    public static int IndexOf(this string str, char c, StringComparison comparison)
+        => str.IndexOf(c.ToString(), comparison);
 
     public static bool StartsWith(this string str, char c)
         => str.StartsWith(c.ToString(), StringComparison.Ordinal);
