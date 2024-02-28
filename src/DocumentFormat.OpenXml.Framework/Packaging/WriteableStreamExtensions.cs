e4c1af533e4729aaf41e505a148492e08faf5f8e
Taylor Southwick <tasou@microsoft.com>
2024-02-27 16:59:12 -0800

    add comment

--- src/DocumentFormat.OpenXml.Framework/Packaging/WriteableStreamExtensions.cs
+++ src/DocumentFormat.OpenXml.Framework/Packaging/WriteableStreamExtensions.cs
@@ -2,53 +2,36 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using DocumentFormat.OpenXml.Features;
-using System;
 using System.IO;
 
 namespace DocumentFormat.OpenXml.Packaging;
 
 internal static class WriteableStreamExtensions
 {
+    private const int DefaultBufferSize = 4096;
+
     /// <summary>
     /// Attempts to replace the underlying stream so that we can modify it even in readonly situations via a copy
     /// </summary>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "Disposable is registered with package")]
     public static bool EnableWriteableStream(this IFeatureCollection features)
     {
         if (features.Get<IPackageStreamFeature>() is { Stream: { CanWrite: false, CanSeek: true } } feature &&
             features.Get<IPackageFeature>() is { } packageFeature &&
             packageFeature.Capabilities.HasFlagFast(PackageCapabilities.Reload))
         {
-            var tempStream = new TemporaryFile();
+            var tempStream = new FileStream(Path.GetTempFileName(), FileMode.Create, FileAccess.ReadWrite, FileShare.None, DefaultBufferSize, FileOptions.DeleteOnClose);
 
             feature.Stream.Position = 0;
-            feature.Stream.CopyTo(tempStream.Stream);
+            feature.Stream.CopyTo(tempStream);
 
-            tempStream.Stream.Position = 0;
-            feature.Stream = tempStream.Stream;
+            tempStream.Position = 0;
 
-            features.GetRequired<IDisposableFeature>().Register(tempStream);
+            // IPackageStreamFeature will take ownership of the stream it is supplied.
+            feature.Stream = tempStream;
 
             return true;
         }
 
         return false;
-    }
-
-    private sealed class TemporaryFile : IDisposable
-    {
-        private const int DefaultBufferSize = 4096;
-
-        public TemporaryFile()
-        {
-            Stream = new FileStream(Path.GetTempFileName(), FileMode.Create, FileAccess.ReadWrite, FileShare.None, DefaultBufferSize, FileOptions.DeleteOnClose);
-        }
-
-        public Stream Stream { get; }
-
-        public void Dispose()
-        {
-            Stream.Dispose();
-        }
     }
 }
