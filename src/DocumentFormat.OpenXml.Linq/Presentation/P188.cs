de3597e18ed4763de9308056115b1acc318ce44c
Tom Jebo <tomjebo@microsoft.com>
2024-07-23 16:29:13 -0700

    added shim for borked p188:ext

---
+++ src/DocumentFormat.OpenXml.Linq/Presentation/P188.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace DocumentFormat.OpenXml.Linq
+{
+    public static partial class P188
+    {
+        /// <summary>
+        /// Represents the p188:ext XML element.
+        /// </summary>
+        /// <remarks>
+        /// <para>As an XML element, it:</para>
+        /// <list type="bullet">
+        /// <item><description>has the following parent XML elements: <see cref="extLst" />.</description></item>
+        /// <item><description>has the following child XML elements: <see cref="P223.reactions" />, <see cref="P228.taskDetails" />.</description></item>
+        /// <item><description>corresponds to the following strongly-typed classes: CommentPropertiesExtension.</description></item>
+        /// </list>
+        /// </remarks>
+        [Obsolete("Unused property, prefer DocumentFormat.OpenXml.Linq.P.ext", true)]
+        public static readonly XName ext = p188 + "ext";
+    }
+}
---
+++ src/DocumentFormat.OpenXml/Schema/Presentation/CommentPropertiesExtension.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocumentFormat.OpenXml.Office2021.PowerPoint.Comment
+{
+    /// <summary>
+    /// <para>Data for the Windows platform..</para>
+    /// <para>This class is available in Office 2021 and above.</para>
+    /// <para>When the object is serialized out as xml, it's qualified name is p:ext.</para>
+    /// </summary>
+    /// <remark>
+    /// <para>The following table lists the possible child types:</para>
+    /// <list type="bullet">
+    ///   <item><description><see cref="DocumentFormat.OpenXml.Office.PowerPoint.Y2022.M03.Main.Reactions" /> <c>&lt;p223:reactions></c></description></item>
+    ///   <item><description><see cref="DocumentFormat.OpenXml.Office.PowerPoint.Y2022.M08.Main.TaskDetails" /> <c>&lt;p228:taskDetails></c></description></item>
+    /// </list>
+    /// </remark>
+    [Obsolete("Unused type, prefer DocumentFormat.OpenXml.Presentation.CommentPropertiesExtension", true)]
+    public partial class CommentPropertiesExtension : DocumentFormat.OpenXml.Presentation.CommentPropertiesExtension
+    {
+        /// <summary>
+        /// Initializes a new instance of the CommentPropertiesExtension class.
+        /// </summary>
+        public CommentPropertiesExtension() : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the CommentPropertiesExtension class with the specified child elements.
+        /// </summary>
+        /// <param name="childElements">Specifies the child elements.</param>
+        public CommentPropertiesExtension(IEnumerable<OpenXmlElement> childElements) : base(childElements)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the CommentPropertiesExtension class with the specified child elements.
+        /// </summary>
+        /// <param name="childElements">Specifies the child elements.</param>
+        public CommentPropertiesExtension(params OpenXmlElement[] childElements) : base(childElements)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the CommentPropertiesExtension class from outer XML.
+        /// </summary>
+        /// <param name="outerXml">Specifies the outer XML of the element.</param>
+        public CommentPropertiesExtension(string outerXml) : base(outerXml)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override OpenXmlElement CloneNode(bool deep) => CloneImp<CommentPropertiesExtension>(deep);
+    }
+}
