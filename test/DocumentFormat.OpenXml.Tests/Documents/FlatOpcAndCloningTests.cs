5ac670fc0fbc47348c5094d1f4744b1373af82eb
Taylor Southwick <twsouthwick@outlook.com>
2024-11-19 09:29:30 -0800

    add header

--- test/DocumentFormat.OpenXml.Tests/Documents/FlatOpcAndCloningTests.cs
+++ test/DocumentFormat.OpenXml.Tests/Documents/FlatOpcAndCloningTests.cs
@@ -1,4 +1,7 @@
-﻿using DocumentFormat.OpenXml.Experimental;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using DocumentFormat.OpenXml.Experimental;
 using DocumentFormat.OpenXml.Packaging;
 using System;
 using System.Collections.Generic;
