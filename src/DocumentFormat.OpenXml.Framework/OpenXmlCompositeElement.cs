738b2f2ab0571d94a9f8890ad222456e07382b45
Michael Bowen <10384982+mikeebowen@users.noreply.github.com>
2025-04-04 15:53:10 -0700

    don't edit openxmlcompositelement

--- src/DocumentFormat.OpenXml.Framework/OpenXmlCompositeElement.cs
+++ src/DocumentFormat.OpenXml.Framework/OpenXmlCompositeElement.cs
@@ -3,6 +3,7 @@
 
 using DocumentFormat.OpenXml.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
